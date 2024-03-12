ccab161dec9a83d6539ce1a40f1b50415fa12b31
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-03-12 17:05:03 +0900

    [level 1] Title: 덧칠하기, Time: 1.48 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        int max = 0;
+        // section 순회
+        for(int i=0; i<section.length; i++) {
+            if(section[i] < max) {
+                continue;
+            }
+            
+            max = section[i] + m;
+            answer++;
+        }
+        return answer;
+    }
+}
