398438d656a10cd2d78ef59bac41eea9ae40dc7f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-05-28 13:15:00 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        // 초기화 
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<want.length; i++) {
+            map.put(want[i], number[i]);
+        }
+        
+        // 10번 계산
+        for(int i=0; i<10; i++) {
+            if(map.containsKey(discount[i])) {  
+                map.put(discount[i], map.get(discount[i]) -1);              
+            }
+        }
+        
+        if(check(map)) {
+            answer++;
+        }
+       
+        int p1 = 0;
+        int p2 = 10; 
+        while(p2 <= discount.length-1) {
+            
+            if(map.containsKey(discount[p1])) {
+                map.put(discount[p1], map.get(discount[p1]) + 1);
+            }
+            
+            if(map.containsKey(discount[p2])) {
+                map.put(discount[p2], map.get(discount[p2]) - 1);
+            }
+            
+            if(check(map)) {
+                answer++;
+            }
+            p1++;
+            p2++;
+        }
+        return answer;
+    }
+    
+    public boolean check(Map<String, Integer> map) {
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() !=0) {
+                return false;
+            }
+        }
+        return true;
+    }        
+}
