f347c4dcc81b490d7e26bb6a989a562937238682
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-05-29 14:07:37 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,36 @@
+class Solution {
+    int max;
+    public int solution(int k, int[][] dungeons) {
+        
+        boolean[] visited = new boolean[dungeons.length];
+        
+        DFS(k, dungeons, visited, 0);
+        return max;
+    }
+    
+    public void DFS(int k, int[][] dungeons, boolean[] visited, int cnt) {
+        if(cnt == dungeons.length || k == 0) {
+            max = Math.max(max, cnt);
+            return;
+        }
+        
+        boolean isfalse = false;
+        // 1~8번째 던전 
+        for(int i=0; i<dungeons.length; i++) {
+            // 던전을 돌 수 있으면 들어가기
+            if(visited[i]) {
+                continue;
+            }
+            
+            if(k >= dungeons[i][0]) {
+                visited[i] = true;
+                DFS(k-dungeons[i][1], dungeons, visited, cnt+1);
+                visited[i] = false;
+                isfalse = true;
+            }
+        }
+        if(!isfalse) {
+            max = Math.max(max, cnt);
+        }
+    }
+}
