d5613546accc9e475b5e4788883bcd400b86c9c7
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-05-30 16:29:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.17 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,33 +3,33 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        List<Character> list = new ArrayList<>();
+        // List<Character> list = new ArrayList<>();
              
         for(int i=0; i<s.length(); i++) {
-            if(i==0) {
-                for(char c : s.toCharArray()) {
-                    list.add(c);
-                }
-            }
+            // if(i==0) {
+            //     for(char c : s.toCharArray()) {
+            //         list.add(c);
+            //     }
+            // }
             
-            if(rotate(list)) {
+            if(rotate(s, i)) {
                 answer++;
             }
             
-            char tmp = list.remove(0);
-            list.add(tmp);
+            // char tmp = list.remove(0);
+            // list.add(tmp);
         }
         
         return answer;
     }
     
-    public boolean rotate(List<Character> list) {
+    public boolean rotate(String s, int idx) {
         
-        List<Character> copy = new ArrayList<>(list);
+        // List<Character> copy = new ArrayList<>(list);
         Stack<Character> stack = new Stack<>();
         
-        while(!copy.isEmpty()) {
-            char c = copy.remove(0);
+        for(int i=idx; i< idx + s.length(); i++) {
+            char c = s.charAt(i%s.length());
             
             if(c == '[' || c == '(' || c == '{') {
                 stack.push(c);
