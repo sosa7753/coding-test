fee0742893d1b699bab23273707303a3e8c5e0f6
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-05-30 16:20:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.32 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,64 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        List<Character> list = new ArrayList<>();
+             
+        for(int i=0; i<s.length(); i++) {
+            if(i==0) {
+                for(char c : s.toCharArray()) {
+                    list.add(c);
+                }
+            }
+            
+            if(rotate(list)) {
+                answer++;
+            }
+            
+            char tmp = list.remove(0);
+            list.add(tmp);
+        }
+        
+        return answer;
+    }
+    
+    public boolean rotate(List<Character> list) {
+        
+        List<Character> copy = new ArrayList<>(list);
+        Stack<Character> stack = new Stack<>();
+        
+        while(!copy.isEmpty()) {
+            char c = copy.remove(0);
+            
+            if(c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+                continue;
+            }
+            
+            if(stack.isEmpty()) {
+                return false;
+            }
+            
+            if(!save(stack.peek(), c)) {
+                return false;
+            }
+            stack.pop();
+        }
+        
+        if(!stack.isEmpty()) {
+            return false;
+        }
+        return true;     
+    }
+    
+    // 맞는 괄호 찾기 
+    public boolean save(char a, char b) {
+        if((a == '[' && b == ']') || (a == '(' && b == ')') || 
+           (a == '{' && b == '}')) {
+            
+            return true;
+        }
+        return false;
+    }
+}
