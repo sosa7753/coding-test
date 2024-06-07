dceede3023c07785242db6c94e25bdf88beeed1e
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-06-07 21:27:35 +0900

    [level 2] Title: 배달, Time: 1.79 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,43 +1,43 @@
 import java.util.*;
 class Solution {
-    List<List<Node>> list;
+    List<List<Node>> list = new ArrayList<>();
     int[] dist;
     public int solution(int N, int[][] road, int K) {
+        int answer = 0;
         
-        // 거리 배열 생성 
-        dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-        // 리스트 생성 
-        list = new ArrayList<>();
+        // 리스트 생성
         for(int i=0; i<=N; i++) {
             list.add(new ArrayList<>());
         }
         
-        // 데이터 초기화
+        // 거리 배열 초기화
+        dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        // 간선 정보 넣어주기.
         for(int i=0; i<road.length; i++) {
             list.get(road[i][0]).add(new Node(road[i][1], road[i][2]));
             list.get(road[i][1]).add(new Node(road[i][0], road[i][2]));
         }
         
-        Dijkstra(N, 1);
+        Dijkstra(N);
         
-        int answer = 0;
         for(int i=1; i<=N; i++) {
             if(dist[i] <= K) {
                 answer++;
             }
         }
-        System.out.println(Arrays.toString(dist));
+        
+
         return answer;
     }
     
-    public void Dijkstra(int N, int start) {    
+    public void Dijkstra(int N) {
         boolean[] visited = new boolean[N+1];
-        PriorityQueue<Node> pq = new PriorityQueue<>((x,y) -> (x.weight - y.weight));
+        PriorityQueue<Node> pq = new PriorityQueue<>((x,y) -> (x.weight-y.weight));
         
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
+        pq.offer(new Node(1, 0));
+        dist[1] = 0;
         
         while(!pq.isEmpty()) {
             Node now = pq.poll();
@@ -48,15 +48,11 @@ class Solution {
             visited[now.to] = true;
             
             for(int i=0; i<list.get(now.to).size(); i++) {
-                Node node = list.get(now.to).get(i);
+                Node adjNode = list.get(now.to).get(i);
                 
-                if(visited[node.to]) {
-                    continue;
-                }
-                               
-                if(dist[node.to] > now.weight + node.weight) {
-                    dist[node.to] = now.weight + node.weight;
-                    pq.offer(new Node(node.to, dist[node.to]));
+                if(dist[adjNode.to] > now.weight + adjNode.weight) {
+                    dist[adjNode.to] = now.weight + adjNode.weight;
+                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
                 }
             }
         }
