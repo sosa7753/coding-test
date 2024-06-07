62c976b40331c09af1fb4b23c55e3f6fa8512509
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-06-07 13:42:42 +0900

    [level 2] Title: 구명보트, Time: 8.71 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int p1 = 0;
+        int p2 = people.length - 1;
+        
+        while(p1 <= p2) {
+            if(people[p1] + people[p2] <= limit) {
+                p1++;
+                p2--;
+            }else {
+                p2--;
+            }         
+            answer++;
+        }        
+        return answer;
+    }
+}
