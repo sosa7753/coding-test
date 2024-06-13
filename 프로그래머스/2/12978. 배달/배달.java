98095b30ab88836193f81024afba4062eb87c0cb
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-06-13 18:14:38 +0900

    [level 2] Title: 배달, Time: 1.31 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,26 +1,20 @@
 import java.util.*;
 class Solution {
-    List<List<Node>> list = new ArrayList<>();
     int[] dist;
+    List<List<int[]>> list = new ArrayList<>();
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         
-        // 리스트 생성
         for(int i=0; i<=N; i++) {
             list.add(new ArrayList<>());
         }
         
-        // 거리 배열 초기화
-        dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-        // 간선 정보 넣어주기.
         for(int i=0; i<road.length; i++) {
-            list.get(road[i][0]).add(new Node(road[i][1], road[i][2]));
-            list.get(road[i][1]).add(new Node(road[i][0], road[i][2]));
+            list.get(road[i][0]).add(new int[] {road[i][1], road[i][2]});
+            list.get(road[i][1]).add(new int[] {road[i][0], road[i][2]});
         }
         
-        Dijkstra(N);
+        Dijkstra(N, road);
         
         for(int i=1; i<=N; i++) {
             if(dist[i] <= K) {
@@ -28,42 +22,34 @@ class Solution {
             }
         }
         
-
         return answer;
     }
     
-    public void Dijkstra(int N) {
+    public void Dijkstra(int N, int[][] road) {
         boolean[] visited = new boolean[N+1];
-        PriorityQueue<Node> pq = new PriorityQueue<>((x,y) -> (x.weight-y.weight));
+        dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         
-        pq.offer(new Node(1, 0));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[1] - y[1]));
         dist[1] = 0;
+        pq.offer(new int[] {1, 0});
         
         while(!pq.isEmpty()) {
-            Node now = pq.poll();
+            int[] now = pq.poll();
             
-            if(visited[now.to]) {
+            if(visited[now[0]]) {
                 continue;
             }
-            visited[now.to] = true;
+            visited[now[0]] = true;
             
-            for(int i=0; i<list.get(now.to).size(); i++) {
-                Node adjNode = list.get(now.to).get(i);
+            for(int i=0; i<list.get(now[0]).size(); i++) {
+                int[] next = list.get(now[0]).get(i);
                 
-                if(dist[adjNode.to] > now.weight + adjNode.weight) {
-                    dist[adjNode.to] = now.weight + adjNode.weight;
-                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+                if(dist[next[0]] > now[1] + next[1]) {
+                    dist[next[0]] = now[1] + next[1];              
                 }
+                pq.offer(new int[] {next[0], dist[next[0]]});
             }
         }
-    }
-}
-
-class Node {
-    int to;
-    int weight;
-    Node(int to, int weight) {
-        this.to = to;
-        this.weight = weight;
     }
 }
