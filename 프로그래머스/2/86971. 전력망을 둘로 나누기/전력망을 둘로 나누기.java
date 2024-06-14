e9ea43f013f8bbaea6b3b06dea7abfc96876643f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-06-14 17:01:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.58 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,57 +1,50 @@
+// 간선정보를 하나씩 끊기.
+// 끊은 연결리스트 가지고 DFS로 개수 세기 
+// 방문배열로 이미 방문한 적 있는 곳은 패스 
 import java.util.*;
 class Solution {
-    List<ArrayList<Integer>> list;
+    List<List<Integer>> list = new ArrayList<>();
+    boolean[] visited;
     int min;
     public int solution(int n, int[][] wires) {
-        
-        list = new ArrayList<>();
         min = Integer.MAX_VALUE;
-             
+        
         for(int i=0; i<=n; i++) {
             list.add(new ArrayList<>());
         }
         
         for(int i=0; i<wires.length; i++) {
             list.get(wires[i][0]).add(wires[i][1]);
+            list.get(wires[i][1]).add(wires[i][0]);            
+        }
+        
+        // 하나씩 끊어서 DFS 돌리기 
+        for(int i=0; i<wires.length; i++) {
+            list.get(wires[i][0]).remove(Integer.valueOf(wires[i][1]));
+            list.get(wires[i][1]).remove(Integer.valueOf(wires[i][0]));
+            
+            visited = new boolean[n+1];
+            
+            int cnt = DFS(1, visited);
+            min = Math.min(min,  Math.abs(cnt - (n - cnt)));
+            
+            list.get(wires[i][0]).add(wires[i][1]);
             list.get(wires[i][1]).add(wires[i][0]);
         }
         
-        
-        // 각 간선을 끊어서 DFS 돌리기
-        for(int i=0; i<wires.length; i++) {
-            int first = wires[i][0];
-            int second = wires[i][1];
-            
-            boolean[] visited = new boolean[n+1];
-            
-            // 간선 제거
-            list.get(first).remove(Integer.valueOf(second));
-            list.get(second).remove(Integer.valueOf(first));
-            
-            int cnt = DFS(1, visited);
-            int diff = Math.abs((n-cnt) - cnt);
-            
-            min = Math.min(diff, min);
-            
-            // 복구
-            list.get(first).add(second);
-            list.get(second).add(first);
-        }
-        
         return min;
-    } 
+    }
     
-    public int DFS(int v, boolean[] visited) {
-        visited[v] = true;
+    public int DFS(int start, boolean[] visited) {
+        visited[start] = true;
         
         int cnt = 1;
         
-        int tmp = list.get(v).size();
-        for(int i=0; i<tmp; i++) {
-            int value = list.get(v).get(i);
-            if(!visited[value]) {
-                cnt += DFS(value, visited);
-            }          
+        for(int i=0; i<list.get(start).size(); i++) {
+            int next = list.get(start).get(i);
+            if(!visited[next]) {
+                cnt += DFS(next, visited);
+            }
         }
         return cnt;
     }
