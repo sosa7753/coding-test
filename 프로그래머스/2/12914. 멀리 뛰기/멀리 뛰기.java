62314ecd4e53e7711984e2025c63d5632b71baa5
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-06-19 10:24:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        if(n==1) {
+            return 1;
+        }else if(n==2) {
+            return 2;
+        }
+        
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] += (dp[i-1]%1234567 + dp[i-2]%1234567)%1234567;          
+        }
+        
+        
+        return dp[n];
+    }
+}
