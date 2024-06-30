6e761cb28e405d4798596ae971f85e796684b29e
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-07-01 00:34:49 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,74 +1,31 @@
 import java.util.*;
 class Solution {
-    // Map에 단어 저장. 
     public int[] solution(int n, String[] words) {
         int[] result = new int[2];
         Map<String, Integer> map = new HashMap<>();
         
-        int[] answer = new int[n+1]; // n번째 사람의 차례 값.
-        for(int i=1; i<=n; i++) {
-            answer[i] = 1;
-        }
-        int cnt = 1;
-        String pre = ""; 
-        boolean check = true;
-        for(int i=0; i<words.length; i++) {
-            if(pre == "") { // 처음이면 패스 
-                pre = words[i];
-                int tmp = cnt%n;
-                if(tmp==0) {
-                    answer[n]++;
-                }else {
-                    answer[tmp]++;
-                }
-                cnt++;        
-                map.put(words[i], 1);
-                continue;
-            }
-            
-            // 끝이 같지 않거나, 같아도 map에 있으면 gg
-            if(pre.charAt(pre.length()-1) != words[i].charAt(0)) {
-                check = false;
-                int a = 0;
-                if(cnt%n == 0) {
-                    a = n;
-                }else {
-                    a = cnt%n;
-                }
-                result[0] = a;
-                result[1] = answer[a];
-                break;
-            } 
-            
-            if(map.containsKey(words[i])) {
-                check = false;
-                int a = 0;
-                if(cnt%n == 0) {
-                    a = n;
-                }else {
-                    a = cnt%n;
-                }
-                result[0] = a;
-                result[1] = answer[a];
-                break;
-            }
-            
-            map.put(words[i], 1);
-            pre = words[i];
-            int b = 0 ;
-            if(cnt%n ==0) {
-                b = n;
-            }else {
-                b = cnt%n;
-            }
-            answer[b]++;
-            cnt++;                  
-        }
+        int[] answer = new int[n]; // n번째 사람의 차례 값.
         
-        if(check) {
-            return new int[] {0, 0};
+        int cnt = 0;
+        String pre = words[0].substring(0,1); 
+        for(int i=0; i<words.length; i++) {
+            // 먼저 차례 높여주기 
+            answer[cnt%n]++;
+            
+            // 끝 값이 다르거나 map에 포함됨.
+            if(pre.charAt(pre.length()-1) != words[i].charAt(0) || 
+               map.containsKey(words[i])) {
+                
+               result[0] = cnt%n + 1;
+               result[1] = answer[cnt%n];
+               return result;             
+            }
+            
+            pre = words[i];
+            map.put(words[i], 1);
+            cnt++;
         }
 
-        return result;
+        return new int[] {0,0};
     }
 }
