6c4b3594cda53e4d42e6bcdba3170427383ed71f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-07-03 11:19:41 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.04 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -5,11 +5,7 @@ class Solution {
 
         int[] arr = new int[food.length];
         for(int i=1; i<food.length; i++) {
-            if(food[i]%2 == 1) {
-                arr[i] = (food[i]-1)/2;
-            }else {
-                arr[i] = food[i]/2;
-            }
+           arr[i] = food[i]/2;
         }
         
         for(int i=1; i<arr.length; i++) {
