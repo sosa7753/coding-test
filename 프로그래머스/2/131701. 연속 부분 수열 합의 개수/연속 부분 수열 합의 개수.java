426149e079894d7877a19f58dc8877e13a5466ee
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-07-05 13:10:02 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 78.37 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,30 +1,15 @@
 import java.util.*;
+
 class Solution {
-    public int solution(int[] elements) {
-        Set<Integer> set = new HashSet<>();
-        
-        int[] S = new int[elements.length];
-        S[0] = elements[0];
-        
-        for(int i=1; i<elements.length; i++) {
-            S[i] = S[i-1] + elements[i];
-        }
-        
-        int max = S[S.length - 1];
-        
-        for(int i=0; i<S.length; i++) {          
-            for(int j=i; j<S.length; j++) {
-                if(i == 0) {
-                    set.add(S[j]);
-                    set.add(max - S[j]);
-                }else {
-                    set.add(S[j] - S[i-1]);
-                    set.add(max - (S[j] - S[i-1]));
+        public int solution(int[] elements) {
+            Set<Integer> set = new HashSet<>();
+            int[] dp = new int[elements.length];
+            for(int len = 1;len <= elements.length; len++){
+                for(int i = 0;i<elements.length;i++){
+                    dp[i] += elements[(len+i-1)%elements.length];
+                    set.add(dp[i]);
                 }
             }
-        }
-        
-        set.remove(0);     
-        return set.size();
+            return set.size();
     }
 }
