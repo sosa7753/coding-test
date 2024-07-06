c447dcfdb0ce30976d97a44cb05576ac89d2ba51
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-07-06 16:15:25 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.48 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        int idx = 0; 
+                
+        // 순위 제거 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<score.length; i++) {
+            pq.offer(score[i]);
+            
+            if(i < k) {
+                answer[idx++] = pq.peek();
+            }else {
+                pq.poll();
+                answer[idx++] = pq.peek();
+            }
+        }
+        return answer;     
+    }
+}
