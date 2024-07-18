8b7a5c1719f8d2e10814ff0f54396bbc9d9df9f3
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-07-18 10:30:10 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -10,20 +10,14 @@ class Solution {
         }
             
         int value = storey%10;
+        int check = (storey%100)/10;
        
         // 0~4 일 때 n만큼 감소, 6~9일 때 10 - n 만큼 감소 
         // 5일 땐 다음 값이 0~4면 +5, 5~9면 10 - n만큼 감소 
-        if(value <= 4) {
+        if(value <= 4 || (value == 5 && check <= 4)) {
             return value + DFS(storey/10);
-        }else if(value >= 6) {
-            return 10 - value + DFS(storey/10 + 1) ;
         }else {
-            int check = (storey%100)/10;
-            if(check <=4) {
-                return value + DFS(storey/10);
-            }else {
-                return value + DFS(storey/10 + 1);
-            }
-        }
+            return 10 - value + DFS(storey/10 + 1);
+        }      
     } 
 }
