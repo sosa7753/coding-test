30381877c63d1918763f783071a06846e62b29a0
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-07-29 14:00:45 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                answer += i + n/i;
+            }
+            
+            if(i*i == n) {
+                answer -= i;
+            }
+        }
+        return answer;
+    }
+}
