e471c4664a25ba72814301a62e0430e3460d985f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-01 09:48:11 +0900

    [level 2] Title: 요격 시스템, Time: 431.65 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,37 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
-        
         int answer = 0;
         
-        // 미사일 시작 기준으로 오름차순 정렬
-        Arrays.sort(targets, (x,y) -> (x[0] - y[0]));
+        // 개구간이 작은순 -> 같다면 폐구간이 작은 순 
+        Arrays.sort(targets, (x,y) -> {
+            if(x[0] == y[0]) {
+                return x[1] - y[1];
+            }else {
+                return x[0] - y[0];
+            }         
+        });
         
-        // 이전 요격 구간 
-        int preStart = targets[0][0]; 
-        int preEnd = targets[0][1];
-        
-        for(int[] target : targets) {          
-            if(answer == 0) {
+        // 범위가 나타날 때마다 범위 줄이기.
+        int min = -1;
+        int max = -1;
+        for(int i=0; i<targets.length; i++) {           
+            // 시작 값이 max이상 이면 answer++; 범위 초기화
+            // 시작 값이 범위 내에 있다면 시작은 새로운 시작 범위, 
+            // 끝 값은 두 미사일 중 작은 값으로 업데이트
+            
+            if(max <= targets[i][0]) {
                 answer++;
+                min = targets[i][0];
+                max = targets[i][1];
                 continue;
             }
             
-            // 현재 미사일 
-            int curStart = target[0];
-            int curEnd = target[1];
-            
-            // 겹치는 요격 범위
-            if(preStart <= curStart && curStart < preEnd) {
-                preStart = curStart;
-                preEnd = Math.min(curEnd, preEnd);
-            }else {
-                answer++;
-                preStart = curStart;
-                preEnd = curEnd;
-            }
+            if(min <= targets[i][0]) {
+                min = targets[i][0];
+                max = Math.min(max, targets[i][1]);
+            }            
         }
-        
+       
         return answer;
-    }    
+    }
 }
