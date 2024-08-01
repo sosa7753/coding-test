b808f84caebee4f2fda2c01d1f0bc716a288b610
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-01 10:24:41 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 16.26 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,60 +1,35 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        List<int[]> list = new ArrayList<>();
-        
         int[] answer = new int[2];
-      
+        
         int n = sequence.length;
+        int p1 = 0;
+        int p2 = 0;
         
-        int p1 = 0; // 시작 포인터
-        int p2 = 0; // 끝 포언터
+        int gap = Integer.MAX_VALUE;
         
-        int sum = sequence[p1];
-        
-        // 합이 k인 부분 수열을 찾는 로직
-        while(p1<=p2 && p1 <= n-1 && p2 <= n-1) {      
-           if(sum < k) { 
-               if(p2 == n-1) {
-                   break;
-               }
-               p2++;
-
-               sum = sum + sequence[p2];
-               continue;
-           }
-           
-           if(sum == k) {
-               list.add(new int[] {p1, p2});
-               
-               if(p2 == n-1) {
-                   break;
-               }
-               p2++;
-               p1++;
-               sum = sum - sequence[p1-1] + sequence[p2];
-               continue;
-           }
-            
-           if(sum > k) {
-               p1++;
-               sum = sum - sequence[p1-1];
-               continue;
-           }         
-        }
-        
-        int m = list.size();
-        int min = Integer.MAX_VALUE;
-        
-        for(int i=0; i<m; i++) {
-            int[] value = list.get(i);
-            
-            if(min > value[1] - value[0]) {
-                min = value[1] - value[0];
-                answer[0] = value[0];
-                answer[1] = value[1];
+        int sum = sequence[0];
+        while(p1 <= p2 && p2 < n) {
+            if(sum < k) {
+                p2++;
+                if(p2 >= n) {
+                    break;
+                }
+                sum += sequence[p2];
+            }else if(sum == k) {
+                if(gap > p2 - p1) {
+                    gap = p2 - p1;
+                    answer[0] = p1;
+                    answer[1] = p2;
+                }
+                sum -= sequence[p1];
+                p1++;
+            }else {
+                sum -= sequence[p1];
+                p1++;
             }
         }
+        
         return answer;
     }
 }
