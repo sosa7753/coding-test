6573be31db96eecff9a5c911863fe3b63d62f1fa
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-04 10:46:15 +0900

    [level 2] Title: 광물 캐기, Time: 14.38 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,59 +1,43 @@
-import java.util.*;
 class Solution {
     int[][] pirodo = {{1,1,1}, {5,1,1}, {25,5,1}};
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
-        
+               
         answer = DFS(picks, minerals, 0);
         return answer;
     }
     
-    public int DFS(int[] picks, String[] minerals, int idx) {
-        
-        // 곡괭이가 없거나 광물이 없을 때 리턴
+    public int DFS(int[] picks, String[] minerals, int cnt) {
         if((picks[0] == 0 && picks[1] == 0 && picks[2] == 0) 
-           || idx >= minerals.length) {
-            
-            return 0;       
+           || cnt >= minerals.length) {
+            return 0;
         }
         
-        int p = picks.length;
-        int m = minerals.length;
-        
         int min = Integer.MAX_VALUE;
-        
-        for(int i=0; i<p; i++) { // 각 곡괭이에 대해
-            // 곡괭이가 없다면 패스
+        for(int i=0; i<3; i++) {
             if(picks[i] == 0) {
                 continue;
             }
             
             int sum = 0;
-            
-            for(int j=idx; j<Math.min(m, idx+5); j++) { // 최대 5번 반복 
-                // 광물에따른 인덱스 변환
-                int col = -1;
+            for(int j=cnt; j<Math.min(cnt+5, minerals.length); j++) {
+                int idx = -1;
                 if("diamond".equals(minerals[j])) {
-                    col = 0;
+                    idx = 0;
                 }else if("iron".equals(minerals[j])) {
-                    col = 1;
+                    idx = 1;
                 }else {
-                    col = 2;
+                    idx = 2;
                 }
                 
-                sum += pirodo[i][col];
+                sum += pirodo[i][idx];
             }
+            picks[i]--;
+            sum += DFS(picks, minerals, cnt+5);
+            min = Math.min(sum, min);
             
-            // 다음 인덱스 DFS 더해주기
-            picks[i] = picks[i] - 1;
-            sum += DFS(picks, minerals, idx + 5);
-            picks[i] = picks[i] + 1;
-            
-            if(sum < min) {
-                min = sum;
-            }
+            picks[i]++;
         }
-        
-        return min;       
+        return min;
     }
 }
