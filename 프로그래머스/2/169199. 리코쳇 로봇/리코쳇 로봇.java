db356ac99e2602f1fe4caec8a11938601513ece5
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-05 09:42:37 +0900

    [level 2] Title: 리코쳇 로봇, Time: 8.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,124 +2,79 @@ import java.util.*;
 class Solution {
     int[] dx = {0, 1, 0, -1};
     int[] dy = {-1, 0, 1, 0};
+    int[] R = new int[2];
+    int[] G = new int[2];
+    boolean[][] visited;
+    int answer = -1;
     public int solution(String[] board) {
-        
         int b = board.length;
-        int bc = board[0].length();
-          
-        int[] start = new int[2];
-        int[] finish = new int[2];
-             
-        // 초기 위치 찾기
-        for(int i=0; i<b; i++) { 
-            for(int j=0; j<bc; j++) {
-                if(board[i].charAt(j) == 'R') { // 시작지점 찾기
-                    start[0] = i;
-                    start[1] = j;
-                    continue;
-                }
-                
-                if(board[i].charAt(j) == 'G') { // 목표지점 찾기
-                    finish[0] = i;
-                    finish[1] = j;
-                    continue;
-                }
-            }
-        }
+        int bl = board[0].length();
         
-        int[][] answer = new int[b][bc]; // 정답 배열 
+        visited = new boolean[b][bl];
+        
         for(int i=0; i<b; i++) {
-            for(int j=0; j<bc; j++) {
-                answer[i][j] = Integer.MAX_VALUE;
+            for(int j=0; j<bl; j++) {
+                if(board[i].charAt(j) == 'R') {
+                    R[0] = i;
+                    R[1] = j;
+                }
+                
+                if(board[i].charAt(j) == 'G') {
+                    G[0] = i;
+                    G[1] = j;
+                }
+                
+                
             }
         }
         
-        answer[start[0]][start[1]] = 0;
-                
-        // 현재 위치 + 횟수 
+        BFS(board);
+        return answer;
+    }
+    
+    public void BFS(String[] board) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {start[0], start[1] , 0});
-        
-        // 방문배열 
-        boolean[][] visited = new boolean[b][bc];
-        visited[start[0]][start[1]] = true;
+        queue.offer(new int[] {R[0], R[1], 0});
         
         while(!queue.isEmpty()) {
             int[] now = queue.poll();
             
-            // 상우하좌 반복
-            for(int i=0; i<4; i++) {
-                
-                int row = now[0] + dy[i];
-                int col = now[1] + dx[i];
-                           
-                // 이동하기
-                while(true) {
-                    
-                    int rowP = row-dy[i];
-                    int colP = col-dx[i];
-                
-                    // 범위를 벗어나는 값이면 
-                    if(row < 0 || row > b-1 || col < 0 || col > bc-1) {
-                        
-                        // 만약 같은 위치면 종료
-                        if(rowP == now[0] && colP == now[1]) {
-                            break;
-                        }
-                        
-                        // 이전 값이 방문한 적 없을 경우 횟수 추가 
-                        if(!visited[rowP][colP]) {
-                            visited[rowP][colP] = true;
-                            answer[rowP][colP] = now[2] + 1;
-                            queue.add(new int[] {rowP, colP, now[2] + 1});
-                            break;                           
-                        }
-                        
-                        // 이전 값이 방문한 적 있을 경우 값이 적을때만 추가 
-                        if(visited[rowP][colP]) {
-                            if(now[2] + 1 < answer[rowP][colP]) {
-                                answer[rowP][colP] = now[2] + 1;
-                                queue.add(new int[] {rowP, colP, now[2]+1}); 
-                            }
-                            break;
-                        }
-                    }
-                    
-                    // 한칸 이동 했을 때 장애물이 아닐 경우
-                    if(board[row].charAt(col) != 'D') {
-                        row += dy[i];
-                        col += dx[i];
-                        continue;
-                    }
-                    
-                    // 장애물일 경우
-                    if(board[row].charAt(col) == 'D') {
-                        
-                        // 이전 칸이 방문한적 없을 때 횟수 기입
-                        if(!visited[rowP][colP]) {
-                            visited[rowP][colP] = true;
-                            answer[rowP][colP] = now[2] + 1;
-                            queue.add(new int[] {rowP, colP, now[2] + 1});
-                            break;
-                        }
-                        
-                        // 이전 칸이 방문한적 있을 때 answer보다 작으면 기입
-                        if(visited[rowP][colP]) {
-                            if(now[2] + 1 < answer[rowP][colP]) {
-                                answer[rowP][colP] = now[2] + 1;
-                                queue.add(new int[] {rowP, colP, now[2] +1});
-                            }
-                            break;
-                        }
-                    }
-                }
+            if(G[0] == now[0] && G[1] == now[1]) { // 도착
+                answer = now[2];
+                return;
             }
-        }
             
-       if(answer[finish[0]][finish[1]] == Integer.MAX_VALUE) {
-                return -1;
-       }   
-        
-       return answer[finish[0]][finish[1]];            
-    } 
+            if(visited[now[0]][now[1]]) {
+                continue;
+            }
+            visited[now[0]][now[1]] = true;
+            
+            // 4방향으로 D가 나오거나 끝까지 가야함.
+            for(int i=0; i<4; i++) {
+                int r = now[0];
+                int c = now[1];
+                while(true) {
+                    r += dy[i];
+                    c += dx[i];
+                    
+                    // 벗어난 값이면
+                    if(r < 0 || r > board.length-1 || 
+                       c < 0 || c > board[0].length()-1) {
+                        
+                        r -= dy[i];
+                        c -= dx[i];
+                        break;
+                    }
+                                        
+                    if(board[r].charAt(c) == 'D') {
+                        r -= dy[i];
+                        c -= dx[i];
+                        break;
+                    } 
+                }
+                
+                queue.offer(new int[] {r, c, now[2] + 1});
+            }           
+        }
+    }
 }
