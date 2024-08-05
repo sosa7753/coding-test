15c8b47e1cac1163310087d37f80eebfeee9d176
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-05 11:59:00 +0900

    [level 1] Title: 달리기 경주, Time: 317.63 ms, Memory: 367 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,32 +2,28 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
         int p = players.length;
-     String[] answer = new String[p];
-    
-     for(int i=0; i<p; i++) {
-         answer[i] = players[i];
-     }
+        String[] answer = new String[p];
         
-     // 현재 순위표
-     Map<String, Integer> mapA = new HashMap<>();
-    
-     // 초기 순위 저장
-     for(int i=0; i<p; i++) {
-        mapA.put(players[i], i);
-     }
-      
-     // 호출만큼 순회
-     for(int i=0; i<callings.length; i++) {
-        int number = mapA.get(callings[i]); // 호출 순위
-        String str = answer[number-1]; // 호출한 사람보다 앞 순위 선수
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<p; i++) {
+            answer[i] = players[i];
+            map.put(players[i], i);
+        }
         
-        answer[number] = str;
-        answer[number-1] = callings[i];
-
-        mapA.put(str, number);
-        mapA.put(callings[i], number-1);
-          
-    }
-    return answer;
+        for(int i=0; i<callings.length; i++) {
+            int now = map.get(callings[i]);
+            
+            String who = answer[now-1];
+            
+            // map 순위 변경
+            map.put(callings[i], now -1);
+            map.put(who, now);
+            
+            // 배열 순위 변경
+            String n = callings[i];
+            answer[now] = who;
+            answer[now-1] = n;                
+        }
+        return answer;
     }
 }
