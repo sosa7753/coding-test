6882bad3211f70d0c4de66349ddc6c44977246cd
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-07 10:05:15 +0900

    [level 2] Title: 무인도 여행, Time: 59.06 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,71 +1,68 @@
 import java.util.*;
 class Solution {
-    boolean[][] visited;
     int[] dx = {0, 1, 0, -1};
     int[] dy = {-1, 0, 1, 0};
-    int n; // 행
-    int m; // 열
-    int cnt;
-    public int[] solution(String[] maps) {        
-        n = maps.length;
-        m = maps[0].length();
+    boolean[][] visited;
+    public int[] solution(String[] maps) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    
+        visited = new boolean[maps.length][maps[0].length()];
         
-        visited = new boolean[n][m];
-        
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(visited[i][j]) {
-                    continue;
-                }
-                
-                if(maps[i].charAt(j) == 'X') {
-                    continue;
-                }
-                cnt = 0;
-                
-                DFS(maps, i,j);
-                list.add(cnt);
+        for(int i=0; i<maps.length; i++) {
+            for(int j=0; j<maps[0].length(); j++) {
+                if(visited[i][j] || maps[i].charAt(j) == 'X') {
+                    continue;                
+                }                
+                pq.offer(DFS(maps, i, j));
             }
         }
-        
-        if(list.isEmpty()) {
+        if(pq.isEmpty()) {
             return new int[] {-1};
         }
-        Collections.sort(list);
-        int[] answer = new int[list.size()];
-        int idx = 0;
-        for(int i=0; i<list.size(); i++) {
-            answer[idx++] = list.get(i);
-        }
         
+        int[] answer = new int[pq.size()];
+        int idx = 0;
+        while(!pq.isEmpty()) {
+            answer[idx++] = pq.poll();
+        }
         return answer;
     }
     
-    public void DFS(String[] maps, int row, int col) {      
-        if(visited[row][col]) {
-            return;
-        }
-        visited[row][col] = true;
-        cnt += maps[row].charAt(col) - '0';
+    public int DFS(String[] maps, int r, int c) {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[] {r, c});
         
-        for(int i=0; i<4; i++) {
-            int r = row + dy[i];
-            int c = col + dx[i];
+        visited[r][c] = true;  
+        int sum = 0;
             
-            if(r < 0 || r > n-1 || c < 0 || c > m-1) {
-                continue;
-            }
+        while(!stack.isEmpty()) {
+            int[] now = stack.pop();
             
-            if(maps[r].charAt(c) == 'X') {
-                continue;
-            }
+            sum += maps[now[0]].charAt(now[1]) - '0';
+            System.out.print(sum +  " ");
             
-            if(visited[r][c]) {
-                continue;
-            }
-            DFS(maps,r,c);
-        }     
+            for(int i=0; i<4; i++) {
+                int row =  now[0] + dy[i];
+                int col =  now[1] + dx[i];
+            
+                if(row < 0 || row > maps.length-1 || 
+                   col < 0 || col > maps[0].length()-1) {
+                    continue;
+                }
+            
+                            
+                if(maps[row].charAt(col) == 'X') {
+                    continue;
+                }
+                
+                if(visited[row][col]) {
+                    continue;
+                }
+                
+                visited[row][col] = true;
+                stack.push(new int[] {row, col});
+            }           
+        }
+        return sum;
     }
 }
