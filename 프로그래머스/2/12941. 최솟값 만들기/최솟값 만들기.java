98f94f05ee5daea7dd5e2d3c72351c377ae8f48f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-07 11:43:41 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.98 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -2,15 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int []A, int []B) {
         int answer = 0;
-
+        
         Arrays.sort(A);
         Arrays.sort(B);
         
-        int min = Math.min(A.length, B.length);
-        for(int i=0; i<min; i++) {
-            answer += A[i] * B[min-1 - i];
+        for(int i=0; i<A.length; i++) {
+            answer += A[i] * B[B.length-1 - i];
         }
-
+        
         return answer;
     }
 }
