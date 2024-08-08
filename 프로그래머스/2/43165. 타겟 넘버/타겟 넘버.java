95ab6c62c0fcde23ceab053d44a7497730b0e9af
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-08 10:30:51 +0900

    [level 2] Title: 타겟 넘버, Time: 6.92 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,19 +3,19 @@ class Solution {
     public int solution(int[] numbers, int target) {
         answer = 0;
         
-        DFS(numbers, target, 0, 0);
+        DFS(numbers, 0, 0, target);
         return answer;
     }
     
-    public void DFS(int[] numbers, int target, int cnt, int now) {
-        if(cnt == numbers.length) {
-            if(target == now) {
-                answer++;
-                return;
+    public void DFS(int[] numbers, int value, int depth, int target) {
+        if(depth == numbers.length) {
+            if(value == target) {
+                answer++;                
             }
             return;
-        }     
-        DFS(numbers, target, cnt+1, now+numbers[cnt]);
-        DFS(numbers, target, cnt+1, now-numbers[cnt]);
+        }
+        
+        DFS(numbers, value+numbers[depth], depth+1, target);
+        DFS(numbers, value-numbers[depth], depth+1, target);        
     }
 }
