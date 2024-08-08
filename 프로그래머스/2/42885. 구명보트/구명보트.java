29ff74526a3e6fe47fd96fde5d29d7eda5343948
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-09 00:17:43 +0900

    [level 2] Title: 구명보트, Time: 10.84 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -6,17 +6,17 @@ class Solution {
         Arrays.sort(people);
         
         int p1 = 0;
-        int p2 = people.length - 1;
-        
-        while(p1 <= p2) {
+        int p2 = people.length -1;
+        while(p1 <= p2 ) {
             if(people[p1] + people[p2] <= limit) {
                 p1++;
                 p2--;
             }else {
                 p2--;
-            }         
+            }
             answer++;
-        }        
+        }
+        
         return answer;
     }
 }
