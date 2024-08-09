271a7aac46ea80a3aa8bcfefccd9fb04fc4d14f1
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-09 10:29:03 +0900

    [level 2] Title: 시소 짝꿍, Time: 142.31 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -14,38 +14,21 @@ class Solution {
                 answer+=prev;
                 continue;
             }
-        
-            int max = binarySearch(weights, weights[i], i+1); // i+1 ~ 범위찾기
-            prev = 0;
             
-            // 구간 내에서 조건에 맞는 짝꿍 찾기 
-            for(int j=i+1; j<max; j++) {
+            prev = 0;
+            for(int j=i+1; j<w; j++) {
+                if(weights[i] * 2 < weights[j]) {
+                    break;
+                }
+                
                 if(weights[i] == weights[j] || weights[i] * 2 == weights[j] ||
                    weights[i] * 3 == weights[j] * 2 || 
                    weights[i] * 4 == weights[j] * 3) {
                     prev++;
-                }
+                }            
             }
-            answer+=prev;
-             
+            answer+=prev;            
         }
         return answer;
-    }
-    
-    // 최대 범위 찾기 
-    public int binarySearch(int[] weights, int num, int i) {
-        int left = i;
-        int right = w-1;
-        
-        while(left <= right) {
-            int mid = left + (right -left)/2;
-            if(weights[mid] > num * 2) {
-                return mid;
-            }else {
-                left = mid + 1;
-            }
-        }
-        
-        return left;
     }
 }
