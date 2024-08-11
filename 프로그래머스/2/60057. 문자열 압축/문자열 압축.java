9653e940e21b2dc0364289a2d814bf14b6dc660a
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-12 00:36:33 +0900

    [level 2] Title: 문자열 압축, Time: 6.91 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,43 +1,51 @@
 class Solution {
+    int answer;
+    int min;
     public int solution(String s) {
-        int answer = s.length();
+        answer = 0;
+        min = Integer.MAX_VALUE;
         
-        for(int i=1; i<=s.length()/2; i++) {
-            answer = Math.min(answer, check(s, i));
+        for(int i=1; i<=s.length(); i++) {
+            DFS(s, "", 0, 1, i);
+            if(answer < min) {
+                min = answer;
+            }
+            answer = 0;
         }
-        
-        return answer;
+        return min;
     }
     
-    public int check(String s, int len) {
-        int result = 0;
-        
-        int max = (s.length()/len) * len; 
-               
-        int count = 1;
-        String start = s.substring(0,len);
-        
-        for(int i=len; i<max; i= i+len) {
-            String str = s.substring(i, i+len);
-            if(start.equals(str)) {
-                count++;
-                continue;
-            }
-            
-            if(count >= 2) {
-                result += String.valueOf(count).length();
-            }
-            count = 1;
-            result += len;
-            start = str;
+    // 총 문자열, 이전문자열, 문자열 시작 인덱스, 반복횟수 , 압축 단위
+    public void DFS(String s, String pre, int idx, int cnt, int len) {
+         
+        // 문자열 범위를 벗어날 경우 
+        if(idx >= s.length()) {  
+            if(cnt >= 2) {
+                answer += String.valueOf(cnt).length();
+            } 
+            return;
         }
         
-        if(count >=2) {
-            result += String.valueOf(count).length();
+        // 마지막 문자열 길이가 다를 경우 
+        if(s.length() - idx < len) {
+            if(cnt >=2) {
+                answer += String.valueOf(cnt).length();
+            }
+            answer += s.length() - idx;
+            return;     
         }
-        result += start.length();
-        result += s.length() - max;  
-        
-        return result;
+             
+        // 이전 값과 같다면 반복횟수 증가 
+        if(pre.equals(s.substring(idx, idx+len))) {
+            DFS(s, s.substring(idx, idx+len), idx+len, cnt+1, len);          
+        }else {
+            // 이전-이전에는 맞는 경우
+            if(cnt >=2) {
+                answer += String.valueOf(cnt).length() + len;
+            }else {
+                answer += len;
+            }
+            DFS(s, s.substring(idx, idx+len), idx+len, 1, len);
+        }
     }
 }
