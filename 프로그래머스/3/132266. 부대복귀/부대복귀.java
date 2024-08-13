3432387d07fb59ecf00a89722f26b462587181d6
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-13 10:23:03 +0900

    [level 3] Title: 부대복귀, Time: 295.67 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,71 +1,56 @@
 import java.util.*;
 class Solution {
-    List<List<Node>> list = new ArrayList<>();
-    int[] dist;
+    int[] dist; 
+    List<List<Integer>> list = new ArrayList<>();
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-
-        // 리스트 생성
+        int[] answer = new int[sources.length];
+        
         for(int i=0; i<=n; i++) {
             list.add(new ArrayList<>());
         }
         
-        // 거리 배열 생성
+        for(int i=0; i<roads.length; i++) {
+            list.get(roads[i][0]).add(roads[i][1]);
+            list.get(roads[i][1]).add(roads[i][0]);
+        }
+        
         dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         
-        // 데이터 저장
-        for(int i=0; i<roads.length; i++) {
-            list.get(roads[i][0]).add(new Node(roads[i][1], 1));
-            list.get(roads[i][1]).add(new Node(roads[i][0], 1));
-        }
-        
-        Dijkstra(n, destination);
-        
-        int[] answer = new int[sources.length];
-        int idx = 0;
+        Dijkstra(destination);
         
         for(int i=0; i<sources.length; i++) {
             if(dist[sources[i]] == Integer.MAX_VALUE) {
-                answer[idx++] = -1;
+                answer[i] = -1;
             }else {
-                answer[idx++] = dist[sources[i]];
+                answer[i] = dist[sources[i]];
             }
         }
-        
         return answer;
     }
     
-    public void Dijkstra(int n, int destination) {
-        boolean[] visited = new boolean[n+1];
-        PriorityQueue<Node> pq = new PriorityQueue<>((x,y) -> (x.weight - y.weight));
-        
-        pq.offer(new Node(destination, 0));
+    public void Dijkstra(int destination) {
+        boolean[] visited = new boolean[dist.length];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[1] - y[1]));
+        pq.offer(new int[] {destination, 0});
         dist[destination] = 0;
         
         while(!pq.isEmpty()) {
-            Node now = pq.poll();
+            int[] now = pq.poll();
             
-            if(visited[now.to]) {
+            if(visited[now[0]]) {
                 continue;
             }
+            visited[now[0]] = true;
             
-            for(int i=0; i<list.get(now.to).size(); i++) {
-                Node node = list.get(now.to).get(i);
+            for(int i=0; i<list.get(now[0]).size(); i++) {
+                int next = list.get(now[0]).get(i);
                 
-                if(dist[node.to] > now.weight + node.weight) {
-                    dist[node.to] = now.weight + node.weight;
-                    pq.offer(new Node(node.to, dist[node.to]));
+                if(dist[next] > now[1] + 1) {
+                    dist[next] = now[1] + 1;
+                    pq.offer(new int[] {next, dist[next]});
                 }
             }
         }
-    }
-}
-
-class Node {
-    int to;
-    int weight;
-    Node(int to, int weight) {
-        this.to = to;
-        this.weight = weight;
     }
 }
