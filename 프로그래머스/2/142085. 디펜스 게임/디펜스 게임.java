c94d6f5e84c0c45749d56bba3d090e171ab78526
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-14 10:00:03 +0900

    [level 2] Title: 디펜스 게임, Time: 93.35 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -3,31 +3,30 @@ class Solution {
     public int solution(int n, int k, int[] enemy) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((x,y) -> (y-x));
         
-        int answer = 0;
-        
-        int b = n;
-        int s = k;
-        
-        for(int i=0; i<enemy.length; i++) {
-            if(b >= enemy[i]) {
+        int answer = 0;     
+        int start = n;
+        for(int i=0; i<enemy.length; i++) {        
+            pq.offer(enemy[i]);
+            
+            if(start >= enemy[i]) {
+                start -= enemy[i];
                 answer++;
-                pq.add(enemy[i]);
-                b -= enemy[i];
                 continue;
             }
-            
-            // 병사로 못막는 경우 
-            if(s > 0) { // 무적권이 있는 경우
-               pq.add(enemy[i]);
-               int tmp = pq.poll();
-               s--;
-               answer++;
-               b = b + tmp - enemy[i];
-            }else {
-                break;
+
+            while(start < enemy[i] && k > 0 && !pq.isEmpty()) {
+                start += pq.poll();
+                k--;
             }
-         }
+            
+            if(start < enemy[i]) {
+                return answer;
+            }else {
+                start -= enemy[i];
+                answer++;
+            }
+        }
         
-        return answer;   
+        return answer;
     }
 }
