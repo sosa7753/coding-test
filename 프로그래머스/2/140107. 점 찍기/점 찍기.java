35d07ca4cb498979fb3d336036af03171705ae66
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-14 10:27:30 +0900

    [level 2] Title: 점 찍기, Time: 14.91 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -2,11 +2,10 @@ class Solution {
     public long solution(int k, int d) {
         long answer = 0;
         
-        // 원과의 거리
-        for(int i=0; i<=d; i = i + k) { // x축 기준 
-            double max = Math.sqrt((double)d*d - (double)i*i);
-            int tmp = (int)((long)max/(long)k);
-            answer += tmp + 1;            
+        for(int i=0; i<=d; i = i + k) {
+            double max = Math.sqrt((long)d * (long)d - (long)i * (long)i);
+            
+            answer += (long)max/k + 1L;
         }
         return answer;
     }
