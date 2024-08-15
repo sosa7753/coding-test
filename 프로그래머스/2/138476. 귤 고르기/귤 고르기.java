b64a1e50a4576a014ae919e6db7e2d4e8762447f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-15 10:57:40 +0900

    [level 2] Title: 귤 고르기, Time: 81.70 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,37 +1,27 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y) -> (y-x));
         int answer = 0;
         
-        int cnt = k;
-        
         Map<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (y[1] - x[1]));
         
-        // Map에 개수 저장 
         for(int i=0; i<tangerine.length; i++) {
             map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
         }
         
-        // 우선순위 큐에 개수가 많은 순으로 저장 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            pq.add(new int[] {entry.getKey(), entry.getValue()});
+        for(int value : map.values()) {
+            pq.offer(value);
         }
         
-        while(!pq.isEmpty()) {
-            int[] now = pq.poll();
+        while(k > 0 && !pq.isEmpty()) {
+            int now = pq.poll();
+            answer++;
             
-            if(cnt == 0) {
+            if(k <= now) {
                 break;
             }
-            
-            if(now[1] >= cnt) {
-                answer++;
-                break;
-            }else {
-                cnt -= now[1];
-                answer++;
-            }            
+            k -= now;
         }
         return answer;
     }
