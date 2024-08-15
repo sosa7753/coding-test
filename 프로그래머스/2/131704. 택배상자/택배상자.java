9613df0d3dcae6857691736a593fb37d80965ef3
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-15 11:16:09 +0900

    [level 2] Title: 택배상자, Time: 38.39 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,25 +2,30 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        int cnt = 0;
-
-        Stack<Integer> 보조 = new Stack<>();
-        Queue<Integer> 기존 = new LinkedList<>();
-
-            for(int i=0; i<order.length; i++){
-                보조.add(i+1);
-
-                while(!보조.isEmpty()){
-                    if(보조.peek() == order[cnt]){
-                        기존.offer(보조.pop());
-                        cnt++;
-                    }else break;
-
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        int now = 1;
+        for(int i=0; i<order.length; i++) {
+            if(now == order[i]) {
+                answer++;
+                now++; 
+            }else if(now > order[i]) {
+                if(stack.peek() != order[i]) {
+                    break;
+                }else {
+                    stack.pop();
+                    answer++;
                 }
+            }else {
+                while(now < order[i]) {
+                    stack.push(now);
+                    now++;
+                }
+                now++;
+                answer++;
             }
-        // System.out.println("보조 : " + 보조.toString());
-        // System.out.println("기존 : " + 기존.toString());
-        answer = 기존.size();
+        }
         return answer;
     }
 }
