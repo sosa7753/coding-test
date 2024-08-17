74724359a522e36183b14a370940e3a890b283a5
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-17 21:53:41 +0900

    [level 2] Title: 할인 행사, Time: 51.71 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,56 +3,46 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        // 초기화 
         Map<String, Integer> map = new HashMap<>();
+        int sum = 0;
         for(int i=0; i<want.length; i++) {
             map.put(want[i], number[i]);
+            sum += number[i];
         }
         
-        // 10번 계산
-        for(int i=0; i<10; i++) {
-            if(map.containsKey(discount[i])) {  
-                map.put(discount[i], map.get(discount[i]) -1);              
-            }
-        }
-        
-        if(check(map)) {
-            answer++;
-        }
-       
-        int p1 = 0;
-        int p2 = 10; 
-        while(p2 <= discount.length-1) {
-            
-            if(map.containsKey(discount[p1])) {
-                map.put(discount[p1], map.get(discount[p1]) + 1);
-            }
-            
-            if(map.containsKey(discount[p2])) {
-                map.put(discount[p2], map.get(discount[p2]) - 1);
+        for(int i=0; i<discount.length; i++) {
+            if(i < sum) {
+                if(map.containsKey(discount[i])) {
+                    map.put(discount[i], map.get(discount[i])-1);
+                }
+                continue;
             }
             
             if(check(map)) {
                 answer++;
             }
-            p1++;
-            p2++;
+            
+            if(map.containsKey(discount[i])) {
+                map.put(discount[i], map.get(discount[i]) - 1);
+            }
+            
+            if(map.containsKey(discount[i-sum])) {
+                map.put(discount[i-sum], map.get(discount[i-sum]) + 1);
+            }
+        }     
+        
+        if(check(map)) {
+            answer++;
         }
         return answer;
     }
     
     public boolean check(Map<String, Integer> map) {
-        // for(Map.Entry<String, Integer> entry : map.entrySet()) {
-        //     if(entry.getValue() !=0) {
-        //         return false;
-        //     }
-        // }
-        
         for(int value : map.values()) {
-            if(value != 0) {
+            if(value > 0) {
                 return false;
             }
         }
         return true;
-    }        
+    }
 }
