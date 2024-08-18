5d5e2f4f14549aa3a4d9f5acf66a51e88d65c8a1
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-18 10:31:34 +0900

    [level 2] Title: 큰 수 만들기, Time: 47.40 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,22 +1,48 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        Stack<Character> stack = new Stack();
-        int len = number.length() - k;
-       
-        for(int i=0; i<number.length(); i++) {
-            char value = number.charAt(i);
-            while(!stack.isEmpty() && k > 0 && stack.peek() < value) {
-                stack.pop();
-                k--;
+        
+        int remove = k;
+        Stack<Character> stack = new Stack<>();
+        
+        int idx = 0;
+        while(idx < number.length() && k > 0) {
+            char c = number.charAt(idx);
+            
+            if(stack.isEmpty()) {
+                stack.push(c);
+                idx++;
+                continue;
             }
-            stack.push(value);
+            
+            char now = stack.peek();
+            if(now >= c) {
+                stack.push(c);
+            }else {
+                while(k > 0 && now < c && !stack.isEmpty()) {
+                    char cc = stack.pop();
+                    k--;
+                    if(!stack.isEmpty()) {
+                        now = stack.peek();
+                    }
+                }
+                stack.push(c);
+            }
+            idx++;
         }
         
         StringBuilder sb = new StringBuilder();
+        int max = stack.size();
+        if(max > number.length() - remove) {
+            for(int i=0; i< max - number.length() + remove; i++) {
+                stack.pop();
+            }
+        }
+        
         while(!stack.isEmpty()) {
             sb.append(stack.pop());
         }
-        return sb.reverse().substring(0,len);   
+        
+        return sb.reverse().toString() + number.substring(idx);
     }
 }
