5ef760c85dce7b222345969b37af04e27054ac21
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-18 11:19:08 +0900

    [level 2] Title: 프로세스, Time: 1.65 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 1.36 ms
+메모리: 77.9 MB, 시간: 1.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 09일 19:56:09
+2024년 08월 18일 11:19:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,50 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
+        int answer = 0;
         
-        // 현재 우선순위가 가장 높은 값 
         PriorityQueue<Integer> pq = new PriorityQueue<>((x,y) -> (y-x));
         
-        // 실행 프로세스를 순서대로 넣어주기
-        Queue<Node> queue = new LinkedList<>();
-        
-        // 우선순위 큐 및 큐 초기화 
+        Queue<int[]> queue = new LinkedList<>();
         for(int i=0; i<priorities.length; i++) {
+            queue.offer(new int[] {priorities[i], i});
             pq.offer(priorities[i]);
-            queue.offer(new Node(i, priorities[i]));
         }
-    
-        // 우선순위가 가장 높은 값이 아니면 다시 큐에 넣기 
-        // 우선순위가 가장 높은 값이면 큐에서 빼기
-        // idx == location인 프로세스의 실행 순서를 알고 싶다.
         
-        int cnt = 0;
         while(true) {
-            Node now = queue.poll(); // 현재 프로세스
-            if(now.priority < pq.peek()) {
-                queue.offer(now);
-                continue;
-            }
+            int[] now = queue.poll();                
             
-            if(location == now.idx) {
-                cnt++;
-                break;
+            // 제거 조건 
+            if(now[0] == pq.peek()) {
+                pq.poll();
+                answer++;
+                if(now[1] == location) {
+                    break;
+                }
             }
-            
-            pq.poll();      
-            cnt++;
+            queue.offer(now);
         }
-        
-        return cnt;
-        
-    }
-}
-
-class Node {
-    int idx;
-    int priority;
-    Node(int idx, int priority) {
-        this.idx = idx;
-        this.priority = priority;
+              
+        return answer;
     }
 }
