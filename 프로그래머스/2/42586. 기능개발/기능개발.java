9904d92d7a1cf356b64cec23119c357ee1add5f9
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-18 11:32:53 +0900

    [level 2] Title: 기능개발, Time: 0.20 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,46 +1,37 @@
 import java.util.*;
-// 큐에 먼저 다 넣기
-// 완료까지 남은 일수로 큐에 넣기 ex) 7 3 9
-// 큐에서 하나 꺼내서, time에 추가 : 0 -> 7
-// peek()값이 time보다 작으면 작지 않은 값까지 꺼내기 
-// 그 개수만큼 배포한 뒤, 반복 
-// time = 7, peek() = 9; -> peek-time만큼 다시 소진하기 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        // 정답을 저장할 리스트 
+    public int[] solution(int[] progresses, int[] speeds) {        
         List<Integer> list = new ArrayList<>();
         
-        // 프로세스 큐 생성 및 초기화
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<progresses.length; i++) {
-            queue.offer((100-progresses[i] + speeds[i]-1)/speeds[i]);
-        }
-         
-        int time = 0;
-        while(!queue.isEmpty()) {
-
-            int remain = queue.poll();
+        Stack<Integer> stack = new Stack<>();
+        
+        int cnt = 0;
+        for(int i=0; i<speeds.length; i++) {
+            int remain = (100 - progresses[i] + speeds[i] -1)/speeds[i]; // 배포일 
             
-            time += remain - time;
-            
-            int cnt = 1;
-            while(!queue.isEmpty()) {
-                if(time >= queue.peek()) { // 이미 완료된 프로세스
-                    queue.poll();
-                    cnt++;
-                    continue;
-                }
-                break;
+            if(stack.isEmpty()) {
+                stack.push(remain);
+                cnt++;
+                continue;
             }
-            list.add(cnt);
+            // 작거나 같다면 cnt++;
+            
+            if(stack.peek() >= remain) {
+                cnt++;
+            }else {
+                list.add(cnt);
+                stack.pop();
+                stack.push(remain);
+                cnt = 1;
+            }           
         }
+        
+        list.add(cnt);
         
         int[] answer = new int[list.size()];
-        int idx = 0;
-        for(int i=0; i<list.size(); i++) {
-            answer[idx++] = list.get(i);
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
         }
-        
         return answer;
     }
 }
