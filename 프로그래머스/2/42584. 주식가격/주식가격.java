df4f969471dd1628b952b8b994d909133c0ec073
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-18 11:56:01 +0900

    [level 2] Title: 주식가격, Time: 58.80 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,49 +1,29 @@
 import java.util.*;
-//int time = 0;
-//맨 처음 stack에 Node(1,0) 넣기 
-//그다음 2 > 1 Node(2, 1);
-//그다음 3 > 2 Node(3, 2);
-//그다음 2 > 3 ? -> Node(3,2) pop; // time - 2; -> 패스 
-//그다음 3 > .....
-//길이가 끝났으면 pop() - time - 
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        Stack<Node> stack = new Stack<>();
+        Stack<int[]> stack = new Stack<>();
         
-        int time = 0;
-        while(time < prices.length) {
+        for(int i=0; i<prices.length; i++) {
             if(stack.isEmpty()) {
-                stack.push(new Node(prices[time], time));
-                time++;
+                stack.push(new int[] {prices[i], i});
                 continue;
             }
-                     
-            if(stack.peek().price <= prices[time]) {  // peek값 보다 큰 경우
-                stack.push(new Node(prices[time], time));
-            }else if(stack.peek().price > prices[time]) { // peek값 보다 작은 경우
-                while(!stack.isEmpty() && stack.peek().price > prices[time]) {
-                    Node node = stack.pop();
-                    answer[node.start] = time - node.start;
-                }
-                stack.push(new Node(prices[time], time));
+                
+            while(!stack.isEmpty() && stack.peek()[0] > prices[i]) {
+                int[] now = stack.pop();
+                answer[now[1]] = i - now[1];
             }
-            time++;             
+            
+            stack.push(new int[] {prices[i], i});
         }
         
         while(!stack.isEmpty()) {
-            Node now = stack.pop();
-            answer[now.start] = prices.length-1 - now.start;
+            int[] now = stack.pop();
+            answer[now[1]] = prices.length -1 - now[1];
         }
+        
         return answer;
-    }
-}
-class Node {
-    int price;
-    int start;
-    Node(int price, int start) {
-        this.price = price;
-        this.start = start;
     }
 }
