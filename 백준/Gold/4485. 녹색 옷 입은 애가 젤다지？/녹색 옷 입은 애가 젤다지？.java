e6d5a265a89f6e54a58abebe103cf2927d764f83
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-22 15:14:41 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 280 ms, Memory: 33576 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int cnt = 0;
+        while(true) {
+            int t = Integer.parseInt(br.readLine());
+            if(t ==0) {
+                break;
+            }
+            
+            cnt++;
+            
+            int[][] map = new int[t][t];
+            for(int i=0; i<t; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<t; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            
+            System.out.println("Problem " + cnt + ": " + DP(map));
+        }              
+    }
+    
+    public static int DP(int[][] map) {
+        int len = map.length;
+        
+        int[][] dist = new int[len][len];
+        for(int i=0; i<len; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0,0});
+        dist[0][0] = map[0][0]; 
+          
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            
+            for(int i=0; i<4; i++) {
+                int row = now[0] + dr[i];
+                int col = now[1] + dc[i];
+                
+                if(row < 0 || row > len-1 || col < 0 || col > len-1) {
+                    continue;
+                }
+                              
+                if(dist[row][col] > dist[now[0]][now[1]] + map[row][col]) {
+                    dist[row][col] = dist[now[0]][now[1]] + map[row][col];              
+                    queue.offer(new int[] {row, col});
+                }                                                    
+            }
+        }
+        return dist[len-1][len-1];   
+    }
+}
