015c2386df192a12e7e5b956dcea2f52368cefd1
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-22 20:47:23 +0900

    [level 2] Title: 호텔 대실, Time: 14.29 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -8,7 +8,7 @@ class Solution {
             return x[0] - y[0];
         });
         
-        PriorityQueue<int[]> pq2 = new PriorityQueue<>((x,y) -> (x[1] - y[1]));
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
         
         for(int i=0; i<book_time.length; i++) {
             pq.offer(new int[] {time(book_time[i][0]), time(book_time[i][1])});
@@ -18,15 +18,15 @@ class Solution {
             int[] now = pq.poll();
             
             if(pq2.isEmpty()) {
-                pq2.offer(now);
+                pq2.offer(now[1]);
                 continue;
             }
             
-            int[] pre = pq2.peek();
-            if(pre[1] + 10 <= now[0]) {
+            int pre = pq2.peek();
+            if(pre + 10 <= now[0]) {
                 pq2.poll();
             }
-            pq2.offer(now);               
+            pq2.offer(now[1]);               
         }
       
         return pq2.size();
