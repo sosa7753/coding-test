ed0eff5375d266a4da6db578eac21a8be23a0612
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-08-28 20:31:12 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 136 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -11,7 +11,6 @@ class Main {
     static int[] dice;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         N = Integer.parseInt(st.nextToken()); // 행
