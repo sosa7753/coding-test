2bc3855c748cccb67cdae7669f2054f3bdaf906b
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-09-16 10:36:11 +0900

    [Gold III] Title: 드래곤 커브, Time: 108 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -1,63 +1,55 @@
 import java.util.*;
 import java.io.*;
 class Main {
-    static int[] dy = {0, -1, 0, 1};
     static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
     static int N;
     static boolean[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         
         N = Integer.parseInt(br.readLine());
-        
         map = new boolean[101][101];
         
         for(int i=0; i<N; i++) {
-            String[] s = br.readLine().split(" ");
-            int x = Integer.parseInt(s[0]);
-            int y = Integer.parseInt(s[1]);
-            int d = Integer.parseInt(s[2]);
-            int g = Integer.parseInt(s[3]);
-            solve(x, y, d, g);
-        }
-        
-        int result = findBox();
-        System.out.println(result);
+            String[] str = br.readLine().split(" ");
+            
+            int x = Integer.parseInt(str[0]);
+            int y = Integer.parseInt(str[1]);
+            int d = Integer.parseInt(str[2]); // 초기 방향 
+            int g = Integer.parseInt(str[3]); // 세대 
+            curve(x,y,d,g);
+        }       
+        System.out.println(square());       
     }
     
-    public static void solve(int x, int y, int d, int g) {
+    public static void curve(int x, int y, int d, int g) {   
         List<Integer> list = new ArrayList<>();
         list.add(d);
+        
         for(int i=1; i<=g; i++) {
             for(int j=list.size()-1; j>=0; j--) {
                 list.add((list.get(j)+1)%4);
             }
         }
-        map[y][x] = true;
+        
+        map[y][x] = true;  
         for(int dir : list) {
             y += dy[dir];
             x += dx[dir];
             map[y][x] = true;
-        }
-    } 
-    public static int findBox(){
-        int result=0;
-        for(int i=0; i<100; i++){
-            for(int j=0; j<100; j++){
-                if(visitDot(i,j)){
+        }     
+    }
+    
+    public static int square() {
+        int result = 0;
+        for(int i=0; i<100; i++) {
+            for(int j=0; j<100; j++) {
+                if(map[i][j] && map[i][j+1] && map[i+1][j] && map[i+1][j+1]) {
                     result++;
                 }
             }
         }
         return result;
-    }
-    public static boolean visitDot(int i, int j){
-        if(map[i][j] && map[i+1][j] && map[i][j+1] && map[i+1][j+1]){
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
