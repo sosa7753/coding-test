949b8b246ea43d65d6f924b6009ae259ad9eab3c
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-09-30 22:46:24 +0900

    [level 3] Title: 단속카메라, Time: 11.71 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,17 +1,32 @@
 import java.util.*;
 class Solution {
-    public int solution(int[][] routes) {   
+    public int solution(int[][] routes) {
         int answer = 0;
-        Arrays.sort(routes, (x,y) -> (x[1]-y[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[0] - y[0]));
         
-        int last = Integer.MIN_VALUE;
-        for(int[] route : routes) {
-            if(last < route[0]) {
-                answer++;
-                last = route[1];
-            }
+        for(int i=0; i<routes.length; i++) {
+            pq.offer(routes[i]);
         }
         
+        int preStart = -40000;
+        int preEnd = -39999;
+        
+        while(!pq.isEmpty()) {
+            int[] now = pq.poll();
+            
+            // 해당 범위에 Start가 포함되는지 
+            if(preStart <= now[0] && now[0] <= preEnd) {
+                preStart = now[0];
+                preEnd = Math.min(preEnd, now[1]);
+                continue;
+            }
+            
+            // 포함이 안된다면, 
+            preStart = now[0];
+            preEnd = now[1];
+            answer++;
+        }
+                 
         return answer;
     }
 }
