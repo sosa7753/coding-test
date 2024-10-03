91e92bd2ab6422b83f3e71577707f7081908e164
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-10-04 00:23:24 +0900

    [level 3] Title: 네트워크, Time: 0.40 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    int[] parents;
+    public int solution(int n, int[][] computers) {
+        parents = new int[n];
+        for(int i=0; i<parents.length; i++) {
+            parents[i] = i;
+        }
+        
+        for(int i=0; i<computers.length; i++) {
+            for(int j=i+1; j<computers.length; j++) {              
+                if(i != j && computers[i][j] == 1) {
+                    union(i, j);
+                }
+            }            
+        }
+             
+        int answer = 0;
+        for(int i=0; i<parents.length; i++) {
+            if(parents[i] == i) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    public void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if(a < b) { // 작은 거로 업데이트 
+            parents[b] = a;       
+        }else {
+            parents[a] = b;
+        }
+    }
+    
+    public int find(int a) {
+        if(a == parents[a]) {
+            return a;
+        }
+        return find(parents[a]);
+    }
+}
