05cf9269984782b913996410308c30b012d20790
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-10-25 14:17:40 +0900

    [level 5] Title: 방의 개수, Time: 738.43 ms, Memory: 432 MB -BaekjoonHub

---
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+    int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
+    public int solution(int[] arrows) {
+        int answer = 0;
+        Map<String, Set<Integer>> map = new HashMap<>();
+        // 방이 생기는 조건 
+        // 1. 이미 방문한 정점인 것. 2. 새로 생긴 간선 
+        
+        // 교차점이 생기면 도형이 2개가 생기기 때문에 2배를 움직인다고 가정 -> 처음 움직일 때 교차점 도달시 1개, 교차점에서 끝 점 도달시 1개
+        int curX = 0;
+        int curY = 0;
+        String now = key(curX, curY);
+        map.put(now, new HashSet<>());
+        for(int i=0; i<arrows.length; i++) {
+            for(int j=0; j<2; j++) {
+                int nx = curX + dx[arrows[i]];
+                int ny = curY + dy[arrows[i]];
+                String s = key(nx, ny);
+                              
+                if(!map.containsKey(s)) { // 첫 방문하는 점
+                    map.put(s, new HashSet<>());                    
+                }else if(!map.get(now).contains(arrows[i])) { // 처음 보는 간선
+                    answer++;
+                }
+                
+                map.get(s).add((arrows[i]+4)%8); // 역방향
+                map.get(now).add(arrows[i]); // 정방향
+                curX = nx;
+                curY = ny;
+                now = s;       
+            }
+        } 
+        
+        return answer;
+    }
+    
+    public String key(int x, int y) {
+        return String.format("%d, %d", x, y);
+    }
+}
