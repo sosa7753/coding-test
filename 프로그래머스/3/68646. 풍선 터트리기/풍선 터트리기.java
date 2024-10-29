92d094c015b1abd78aa757e8e2ca8b40b32e5e85
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-10-29 11:24:18 +0900

    [level 3] Title: 풍선 터트리기, Time: 443.77 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,20 +1,15 @@
+import java.util.*;
 class Solution {
     public int solution(int[] a) {
-        int answer = 0;
-        
-        int L = a[0];
-        int[] R = new int[a.length];
-        R[a.length-1] = a[a.length-1];
-        for(int i = R.length-2; i>=1; i--) {
-            R[i] = Math.min(R[i+1], a[i]);
-        }
-        
+        int L = Integer.MAX_VALUE;
+        int R = Integer.MAX_VALUE;
+        Set<Integer> set = new HashSet<>();
         for(int i=0; i<a.length; i++) {
-            if(!(L < a[i] && a[i] > R[i])) {
-                answer++;
-            }
             L = Math.min(L, a[i]);
+            R = Math.min(R, a[a.length-1-i]);
+            set.add(L);
+            set.add(R);
         }
-        return answer;
+        return set.size();
     }
 }
