64f6cc24a4c532531b0fbc19974df17b1f670d79
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2024-11-07 00:24:14 +0900

    [level 3] Title: 징검다리 건너기, Time: 25.27 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,24 +1,23 @@
 class Solution {
     public int solution(int[] stones, int k) {
-        int L = 0;
+        int L = 1;
         int R = 200000000;
         while(L <= R) {
             int mid = (L + R)/2;
             
-            int cnt = 0;
             if(check(stones, mid, k)) {
                 L = mid+1;
             }else {
                 R = mid-1;
             }
         }
-        return L;
+        return R;
     }
     
     public boolean check(int[] stones, int mid, int k) {
         int cnt = 0;
         for(int i=0; i<stones.length; i++) {
-            if(stones[i] - mid <= 0) {
+            if(stones[i] < mid) {
                 cnt++;
                 if(cnt == k) {
                     return false;
