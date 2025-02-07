e8e855c5076d246adc05a8cacfea475b342de18e
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-02-07 22:35:03 +0900

    [level 4] Title: 쿠키 구입, Time: 13.36 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.java
+++ 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.java
@@ -2,45 +2,34 @@ class Solution {
     public int solution(int[] cookie) {
         int answer = 0;
         
-        int[] S = new int[cookie.length];
-        S[0] = cookie[0];
+        int[] S = new int[cookie.length+1];       
         for(int i=1; i<S.length; i++) {
-            S[i] = S[i-1] + cookie[i];
+            S[i] = S[i-1] + cookie[i-1];
         }
-        
-        for(int i=0; i<S.length-1; i++) { // 시작
-            for(int j=i+1; j<S.length; j++) { // 끝 
-                int check = 0;
-                if(i == 0) {
-                    check = S[j];
-                }else {
-                    check = S[j] - S[i-1];
-                }
                 
-                if(check%2 == 1 || answer * 2 > check) {
+        for(int i=1; i<S.length-1; i++) {
+            for(int j=i+1; j<S.length; j++) {
+                int sum = S[j] - S[i-1];              
+                if(sum%2 == 1 || answer * 2 > sum) {
                     continue;
                 }
                 
                 int L = i;
-                int R = j;                
-                while(L <= R) {
+                int R = j;
+                while(L<=R) {
                     int mid = (L + R)/2;
-                    int tmp = 0;
-                    if(mid == 0) {
-                        tmp = S[j];
-                    }else {
-                        tmp = S[j]-S[mid-1];
+                    int check = S[j] - S[mid-1];
+                    if(check == sum/2) {
+                        answer = sum/2;
+                        break;
                     }
                     
-                    if(check/2 == tmp) {
-                        answer = check/2;
-                        break;
-                    }else if(check/2 > tmp) {
+                    if(check < sum/2) {
                         R = mid-1;
                     }else {
                         L = mid+1;
                     }
-                }
+                }               
             }
         }
         return answer;
