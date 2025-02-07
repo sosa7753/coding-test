a81adf6119f57606d7233e417610e28c26163c21
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-02-07 12:07:38 +0900

    [level 4] Title: 트리 트리오 중간값, Time: 472.55 ms, Memory: 187 MB -BaekjoonHub

--- 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.java
+++ 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.java
@@ -3,9 +3,7 @@ class Solution {
     List<List<Integer>> list = new ArrayList<>();
     int cnt = 0;
     int max = 0;
-    public int solution(int n, int[][] edges) {
-        int answer = 0;
-        
+    public int solution(int n, int[][] edges) {      
         for(int i=0; i<=n; i++) {
             list.add(new ArrayList<>());
         }
@@ -15,48 +13,46 @@ class Solution {
             list.get(edges[i][1]).add(edges[i][0]);
         }
         
-        // 3번 탐색. 가장 먼 정점 찾기 -> 다른 정점 찾기 -> 지름 경로 개수 찾기        
+        // 3번 탐색. 가장 먼 정점 찾기 -> 다른 정점 찾기 -> 지름 경로 개수 찾기
         int node = edges[0][0];
         for(int i=0; i<3; i++) {
             node = BFS(node);
-            if(i==0 || cnt <=1) {
+            if(i == 0 || cnt <=1) {
                 cnt = 0;
                 continue;
             }else {
                 return max;
             }
         }                
-        return max - 1;
+        return max -1;
     }
-    
     public int BFS(int node) {
         boolean[] visited = new boolean[250001];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(node);
-        queue.offer(0);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{node, 0}); // 현재 점, 깊이
         visited[node] = true;
         
         int last = 0;
         while(!queue.isEmpty()) {
-            int now = queue.poll();
-            last= now;
-            int level = queue.poll();
-                        
+            int[] tmp = queue.poll();
+            int now = tmp[0];
+            last = now;
+            int dep = tmp[1];
+            
             for(int i=0; i<list.get(now).size(); i++) {
                 int next = list.get(now).get(i);
-                
                 if(!visited[next]) {
-                    queue.offer(next);
-                    queue.offer(level+1);
-                    if(level+1 == max) {
+                    queue.offer(new int[] {next, dep+1});
+                    
+                    if(dep+1 == max) {
                         cnt++;
-                    }else {
+                    }else if(dep+1 > max) {
                         cnt = 1;
-                        max = level + 1;
+                        max = dep+1;
                     }
                     visited[next] = true;
                 }
-             }
+            }
         }
         return last;
     }
