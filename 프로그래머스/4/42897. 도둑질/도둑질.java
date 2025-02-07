036db1de265ae6c35fed9b310d546069fecc22b3
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-02-07 14:56:32 +0900

    [level 4] Title: 도둑질, Time: 28.01 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int solution(int[] money) {
-        int[][] dp = new int[2][money.length];
+    public int solution(int[] money) {        
+        int[][] dp = new int[2][money.length];  // 0은 첫집을 털음. 1은 첫집을 안털음.
         
-        // 1은 첫집을 털음. 2는 안털음.
-        dp[1][0] = money[0];
-        dp[1][1] = money[0];
+        dp[0][0] = money[0];
+        dp[0][1] = money[0];
         
-        dp[0][0] = 0;
-        dp[0][1] = money[1];
-        
-        for(int i=2; i<dp[0].length; i++) {
-            dp[1][i] = Math.max(dp[1][i-1], dp[1][i-2] + money[i]);
+        dp[1][1] = money[1];
+    
+        for(int i=2; i<money.length; i++) {
             dp[0][i] = Math.max(dp[0][i-1], dp[0][i-2] + money[i]);
+            dp[1][i] = Math.max(dp[1][i-1], dp[1][i-2] + money[i]);
         }
-        return Math.max(dp[1][money.length-2], dp[0][money.length-1]);
+        return Math.max(dp[0][money.length-2], dp[1][money.length-1]);
     }
 }
