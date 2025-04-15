ab6ba77620cffc0e947f0307087eed248c49b02f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-04-15 15:38:33 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2132 ms, Memory: 611032 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,44 +1,40 @@
 import java.util.*;
 import java.io.*;
 class Main {
-    public static class Node {
-        int value;
-        int idx;
-        Node(int value, int idx) {
-            this.value = value;
-            this.idx = idx;
-        }
-    }
+    static int N, L;
+    static int[] num;
+    static int[] answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
         
-        Deque<Node> dq = new LinkedList<>();
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
         
-        int[] arr = new int[N];
+        num = new int[N];
+        answer = new int[N];
+       
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            num[i] = Integer.parseInt(st.nextToken());
         }
         
+        Deque<int[]> dq = new LinkedList<>();
+        
         for(int i=0; i<N; i++) {
-            while(!dq.isEmpty() && dq.getLast().value > arr[i]) {
+            while(!dq.isEmpty() && dq.getLast()[1] > num[i]) {
                 dq.removeLast();
             }
             
-            dq.addLast(new Node(arr[i], i));
+            dq.addLast(new int[]{i, num[i]});
             
-            if(dq.getFirst().idx <= i-K) {
+            if(dq.getFirst()[0] <= i-L) {
                 dq.removeFirst();
             }
             
-            bw.write(dq.getFirst().value + " ");
+            bw.write(dq.getFirst()[1] + " ");
         }
-        
         bw.flush();
         bw.close();       
     }
