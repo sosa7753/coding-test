21e6bd3d0d0010f1d2cc54c4a3c50aedf216be9c
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-05-12 21:56:59 +0900

    [level 3] Title: 인사고과, Time: 200.01 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,60 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] scores) {
-        int answer = 0;
-        int[] result = new int[200001];
+        int answer = 1;
         
-        int a = scores[0][0];
-        int b = scores[0][1];
-        int value = a+b;
-        boolean ok = false;
-        
-        Stack<int[]> stack = new Stack<>();
-        
+        int[] s = scores[0];
         Arrays.sort(scores, (x,y) -> {
             if(x[0] == y[0]) {
-                return y[1] - x[1];
+                return x[1] - y[1];
             }else {
-                return x[0] - y[0];
+                return y[0] - x[0];
             }
         });
         
-        for(int i=0; i<scores.length; i++) {
-            if(stack.isEmpty()) {
-                stack.push(scores[i]);
+        int max = 0;
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s)) {
+                    return -1;
+                }
                 continue;
             }
             
-            while(!stack.isEmpty() && stack.peek()[0] < scores[i][0] 
-                  && stack.peek()[1] < scores[i][1]) {
-                stack.pop();
+            max = Math.max(max, score[1]);
+            if((s[0] + s[1]) < score[0] + score[1]) {
+                answer++;
             }
-            stack.push(scores[i]);
         }
         
-        int dep = stack.size();
-        for(int i=0; i<dep; i++) {
-            int[] t = stack.pop();
-            if((t[0] == a && t[1] == b) && ok == false) {
-                ok = true;
-            }
-            
-            int idx = t[0] + t[1];
-            result[idx]++;         
-        }
-        
-        if(!ok) {
-            return -1;
-        }
-        
-        for(int i=200000; i>value; i--) {
-            answer += result[i];
-        }
-        
-        if(answer == 0) {
-            return 1;
-        }
-
-        return answer + 1;
+        return answer;
     }
 }
