2a26aea1aed96043e954b1da73f87ca6a00ec6cb
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-05-16 12:38:21 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 37.90 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,33 +1,25 @@
-class Solution
-{
-    public int solution(String s)
-    {
-        int answer =0;
-        int n= s.length();
+class Solution {
+    public int solution(String s) {
+        int answer = 1;        
+        int n = s.length();
         
-        loop:
-        for(int i=n; i>=1; i--){ //i: 펠린드롬 길이. 가장 긴 길이부터 살펴보기
-            for(int j=0; j<=n-i; j++){
-               boolean flag= true;
-               int st=j; //시작 인덱스 
-               int en=j+i-1; // 끝 인덱스 
-               
-               while(st<en){ 
-                   if(s.charAt(st)!=s.charAt(en)){
-                       flag=false;
-                       break;
-                   }
-                   st++;
-                   en--;
-               }
-               if(flag){ // 팰린드롬이면 정답 갱신하고 반복문 중단하기 
-                   answer=i;
-                   break loop;
-               }
-            }
-               
+        boolean[][] dp = new boolean[n][n];
+        for(int i=0; i<n; i++) {
+            dp[i][i] = true;
         }
+        
+        for(int i=2; i<=n; i++) { // 문자열 길이
+            for(int j=0; j<=n-i; j++) { // 시작 인덱스
+                int l = j+i-1; // 끝 인덱스
+                if(s.charAt(j) == s.charAt(l)) {
+                    if(i==2 || dp[j+1][l-1]) {
+                        dp[j][l] = true;
+                        answer = Math.max(answer, i);
+                    }
+                }
+            }
+        }
+
         return answer;
     }
-   
 }
