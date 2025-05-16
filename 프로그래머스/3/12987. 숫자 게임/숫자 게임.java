a5d522c81ae2edac97e600a49ca167b6616d821d
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-05-17 00:04:56 +0900

    [level 3] Title: 숫자 게임, Time: 110.04 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -6,15 +6,12 @@ class Solution {
         Arrays.sort(A);
         Arrays.sort(B);
         
-        int L = 0;
         int R = B.length-1;
         for(int i=A.length-1; i>=0; i--) {
             if(A[i] < B[R]) {
                 answer++;
                 R--;
-                continue;
             }         
-            L++;
         }
         
         return answer;
