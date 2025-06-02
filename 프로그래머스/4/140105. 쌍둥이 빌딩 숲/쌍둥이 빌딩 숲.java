aa92fbcbe988ea3ff00fea34fe1794169e569dd0
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-06-02 21:17:49 +0900

    [level 4] Title: 쌍둥이 빌딩 숲, Time: 3.10 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/4/140105. 쌍둥이 빌딩 숲/쌍둥이 빌딩 숲.java
+++ 프로그래머스/4/140105. 쌍둥이 빌딩 숲/쌍둥이 빌딩 숲.java
@@ -3,11 +3,11 @@ class Solution {
     public int solution(int n, int count) {
         int answer = 0;
         
-        long[][] dp = new long[n+1][count+1]; // 빌딩 종류, 보이는 수 
+        long[][] dp = new long[n+1][count+1]; // 빌딩 종류, 보이는 빌딩 수 
         dp[1][1] = 1;
         
         for(int i=2; i<=n; i++) { // 빌딩 종류
-            for(int j=1; j<=count; j++) { // 보이는 수
+            for(int j=1; j<=Math.min(i, count); j++) { // 보이는 빌딩 수
                 dp[i][j] = (dp[i-1][j] * (i-1) * 2 + dp[i-1][j-1])%MOD;
             }
         }
