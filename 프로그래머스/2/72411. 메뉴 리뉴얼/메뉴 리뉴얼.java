e479399cb431c993861cb19b7928437e6ec4ae68
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-06-09 23:16:46 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 21.45 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -4,109 +4,60 @@ class Solution {
     Map<String, Integer> map;
     int[] max;
     public String[] solution(String[] orders, int[] course) {
-      
+     
         list = new ArrayList<>();
         map = new HashMap<>();
-        
         max = new int[course.length];
         
-        int number = 0;
-        // 최대 알파벳 찾기 
-        for(int i=0; i<orders.length; i++) {
-            for(int j=0; j<orders[i].length(); j++) {
-                int com = orders[i].charAt(j) - 'A';
-                if(com > number) {
-                    number = com;
-                }
+        
+        for(int i=0; i<orders.length; i++) { // 각 order 문자열에 대해
+            char[] c = orders[i].toCharArray();
+            Arrays.sort(c);
+            String str = String.copyValueOf(c);
+            for(int j=0; j<course.length; j++) { // 각 길이 만큼 
+                DFS(0, "", str, course[j], 0, j);              
             }
         }
         
-        for(int i=0; i<course.length; i++) {
-            boolean[] visited = new boolean[number+1];
-            String[] alpa = new String[course[i]];
-            DFS(alpa, orders, visited, 0, course[i], 0, i);
-        }
-        
-        // Map에서 가져와서 계산하기
+        // Map에서 찾기 
         for(String s : map.keySet()) {
             for(int i=0; i<course.length; i++) {
-                if(course[i] == s.length() && max[i] == map.get(s)) {
-                    list.add(s);                  
+                if(course[i] == s.length() && max[i] != 1 
+                   && map.get(s) == max[i]) {
+                    list.add(s);
+                    System.out.print(s + " ");
                 }
             }
         }
         
+        // list 정렬 후 answer에 저장하기 
         Collections.sort(list);
-        int len = list.size();
-        String[] answer = new String[len];
+        int size = list.size();
+        String[] answer = new String[size];
         int idx = 0;
-              
-        for(String str : list) {
-            answer[idx++] = str;
-        }       
+        for(String s : list) {
+            answer[idx++] = s;
+        }
         return answer;
     }
     
-    public void DFS(String[] alpa, String[] orders, boolean[] visited, 
-                    int now, int max, int cnt, int idx) {
-        if(max == cnt) {
-            String tmp = make(alpa);
-            cal(orders, tmp, idx);        
+    // 해당 문자열의 해당 크기만큼 조합을 만들어서 Map에 넣기 
+    // 현재 알파벳 인덱스 ,시작 문자열 , 해당 문자열, 조합 길이, 
+    // 현재 채우는 인덱스, max배열 idx
+    public void DFS(int cur, String start, String str, 
+                    int len, int cnt, int idx) {
+        
+        if(cnt == len) {
+            map.put(start, map.getOrDefault(start, 0) + 1);
+            max[idx] = Math.max(max[idx], map.get(start));
             return;
         }
         
-        // A - Z 
-        for(int i=0; i<visited.length; i++) {            
-            if(i < now) {
-                continue;
-            }
-            
-            if(visited[i]) {
-                continue;
-            }
-            
-            char c = (char)('A' + i);
-            
-            alpa[cnt] = String.valueOf(c);
-            visited[i] = true;
-            DFS(alpa, orders, visited, i, max, cnt+1, idx);
-            visited[i] = false;
-        }
-    }
-    
-    public String make(String[] alpa) {
-        StringBuilder sb = new StringBuilder();
-        
-        for(String al : alpa) {
-            sb.append(al);
-        }
-        return sb.toString(); 
-    }
-    
-    public void cal(String[] orders, String str, int idx) {
-        
-        int result = 0;
-        for(int i=0; i<orders.length; i++) {      
-            
-            boolean isfalse = true;
-            for(int j=0; j<str.length(); j++) {
-                // 주문 메뉴에 포함이 안된 메뉴인 경우 
-                if(!orders[i].contains(str.substring(j,j+1))) {
-                    isfalse = false;
-                    break;
-                }    
-            }
-            
-            if(isfalse) {
-                result++;
-            }
-        }
-        
-        if(result >=2) {
-            map.put(str, result);
-            if(result > max[idx]) {
-                max[idx] = result;
-            }
-        }    
+        for(int i=cur; i<str.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(start);
+            sb.append(str.substring(i,i+1));
+            DFS(i+1, sb.toString(), str, len, cnt+1, idx);
+        }      
     }
 }
