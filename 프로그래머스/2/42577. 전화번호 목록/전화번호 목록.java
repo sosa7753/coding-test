9d498a69e9a96a4b080481479a365b88cff6a0bf
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-06-12 15:17:41 +0900

    [level 2] Title: 전화번호 목록, Time: 321.43 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,33 +1,15 @@
 import java.util.*;
 class Solution {
-    Tree root;
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);     
-        root = new Tree();
         
-        for(String s : phone_book) {
-            if(!insert(s)) {
-                return false;
-            }
-        }       
-        return true;
-    }
-    
-    public boolean insert(String s) {
-        Tree now = root;
-        for(int i=0; i<s.length(); i++) {
-            now = now.map.computeIfAbsent(s.charAt(i), c -> new Tree()); 
-            if(now.last == true) {
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
                 return false;
             }
         }
-        now.last = true;
+        
         return true;
     }
-    
-}
-
-class Tree {
-    Map<Character, Tree> map = new HashMap<>();
-    boolean last = false; 
 }
