9f294cf0d07fd506520c163a8a3d0ef0df5ace44
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-06-16 14:21:10 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 123.61 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.java
+++ 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.java
@@ -1,74 +1,76 @@
+import java.util.*;
 class Solution {
-    int[] dr = {-1, 0, 1, 0};
-    int[] dc = {0, 1, 0, -1};
-    int[] arr; // 방향 배열
+    int[] dr = {0, 1, 0};
+    int[] dc = {1, 0, -1};
+    int[][] clock;
+    int[][] map;
+    int[] arr;
+    int n;
     int answer = Integer.MAX_VALUE;
-    public int solution(int[][] clockHands) {        
-        arr = new int[clockHands[0].length];        
-        DFS(0, clockHands);      
+    public int solution(int[][] clockHands) {      
+        clock = clockHands;
+        n = clockHands.length;      
+        arr = new int[n];
+        
+        DFS(0);
         return answer;
     }
     
-    public void DFS(int cnt, int[][] clockHands) {
-        if(cnt == arr.length) {
-            int result = cal(clockHands);
-            if(result >= 0) {
-                answer = Math.min(answer, result);
-            }
+    public void DFS(int cnt) {
+        if(cnt == n) {
+            cal();
             return;
         }
         
         for(int i=0; i<4; i++) {
             arr[cnt] = i;
-            DFS(cnt+1, clockHands);
-        }        
+            DFS(cnt+1);
+        }   
     }
     
-    public int cal(int[][] clockHands) {
-       int sum = 0;
-       int[][] map = new int[clockHands.length][clockHands[0].length];
-       for(int i=0; i<clockHands.length; i++) {
-           map[i] = clockHands[i].clone();
-       } 
-           
-       // 회전
-       int[] next = arr.clone();
-       for(int i=0; i<map.length; i++) {
-           rotate(map, i, next);
-           for(int j=0; j<map[i].length; j++) {
-               next[j] = (4-map[i][j])%4; 
-               sum += next[j];
-           }
-       }
+    public void cal() {
+        map = new int[n][n];
+        for(int i=0; i<n; i++) {
+            map[i] = Arrays.copyOf(clock[i], n);
+        }
         
-       // 마지막이 풀리는지       
-       for(int i=0; i<next.length; i++) {
-          if(next[i] != 0) {
-              return -1;
-          } 
-       }
-       
-       // arr 값 더해주기.
-       for(int i=0; i<arr.length; i++) {
-           sum += arr[i];
-       }
-       return sum;
+        int result = 0;
+        int[] next = Arrays.copyOf(arr, arr.length); // 다음 배열에 더해줄 값
+        
+        for(int i=0; i<n; i++) {
+           rotate(next, i);
+           if(result >= answer) {
+               return;
+           }
+            
+           for(int j=0; j<n; j++) {                
+               result += next[j];   
+               next[j] = (4-map[i][j])%4;
+           }      
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(next[i] != 0) {
+                return;
+            }
+        }
+
+        answer = Math.min(answer, result);
     }
     
-    public void rotate(int[][] map, int row, int[] change) {        
-        for(int i=0; i<map[row].length; i++) {
-            map[row][i] = (map[row][i] + change[i])%4;
-            
-            for(int j=0; j<4; j++) {
-                int r = row + dr[j];
-                int c = i + dc[j];
+    public void rotate(int[] next, int row) {
+        for(int i=0; i<n; i++) { // 열 반복
+            map[row][i] = (map[row][i] + next[i])%4;
+            for(int j=0; j<3; j++) { // 3방향 탐색(윗 방향 제외)
+                int nr = row + dr[j];
+                int nc = i + dc[j];
                 
-                if(r < 0 || r > map.length-1 || c < 0 || c > map[0].length-1) {
+                if(nr > n-1 || nc < 0 || nc > n-1) {
                     continue;
                 }
                 
-                map[r][c] = (map[r][c] + change[i])%4;
-            }        
+                map[nr][nc] = (map[nr][nc] + next[i])%4;
+            }
         }
     }
 }
