d5665a9b8ae6d2162aa8e43755f9764eb79a06b8
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-07-04 22:40:39 +0900

    [Gold IV] Title: 고냥이, Time: 208 ms, Memory: 19388 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,29 +1,34 @@
 import java.util.*;
 import java.io.*;
 class Main {
+    static int N;
+    static String str;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());       
-        String str = br.readLine();
+       
+        N = Integer.parseInt(br.readLine());
+        str = br.readLine();
         
         int p1 = 0;
         int p2 = 0;
-        
+    
         Map<Character, Integer> map = new HashMap<>();
         int answer = 0;
-        while(p1 <= p2 && p2 < str.length()) {
-            map.put(str.charAt(p2), map.getOrDefault(str.charAt(p2), 0) + 1);
+        while(p1<=p2 && p2<str.length()) {
+            char c = str.charAt(p2);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            
             while(map.size() > N) {
-                map.put(str.charAt(p1), map.get(str.charAt(p1)) -1);
-                if(map.get(str.charAt(p1)) ==0) {
-                    map.remove(str.charAt(p1));
+                char c2 = str.charAt(p1);
+                map.put(c2, map.get(c2) - 1);
+                if(map.get(c2) == 0) {
+                    map.remove(c2);
                 }
                 p1++;
             }
             answer = Math.max(answer, p2 - p1 + 1);
-            p2++;          
+            p2++;
         }
-        System.out.print(answer);
+        System.out.print(answer);    
     }
 }
