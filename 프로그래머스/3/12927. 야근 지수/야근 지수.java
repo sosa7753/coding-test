c6360c7206309ca9fa751b3ac16e66fd55159730
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-07-04 20:52:23 +0900

    [level 3] Title: 야근 지수, Time: 123.75 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,27 +3,23 @@ class Solution {
     public long solution(int n, int[] works) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((x,y) -> (y-x));
         
-        for(int i=0; i<works.length; i++) {
-            pq.offer(works[i]);
-        } 
+        for(int work : works) {
+            pq.offer(work);
+        }
         
         int start = n;
-        while(start > 0 && !pq.isEmpty()) {
+        while(!pq.isEmpty() && start > 0) {
             int now = pq.poll();
             if(now != 1) {
-                pq.offer(now-1);                
+                pq.offer(now-1);
             }
             start--;
         }
         
         long answer = 0;
-        if(pq.isEmpty()) {
-            return answer;
-        }
-        
         while(!pq.isEmpty()) {
-            int now = pq.poll();
-            answer += (long)now * (long)now;
+            long now = pq.poll();
+            answer += now * now;
         }
         
         return answer;
