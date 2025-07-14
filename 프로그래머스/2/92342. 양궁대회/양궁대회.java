2fa3b80dc22a456657215dfc7eae037d6f0ccd61
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-07-14 15:33:47 +0900

    [level 2] Title: 양궁대회, Time: 49.91 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,70 +1,69 @@
 import java.util.*;
 class Solution {
-    int win;
-    int[] answer;
+    int result = 0;
+    int[] now;
     public int[] solution(int n, int[] info) {
-        answer = new int[info.length];
-             
-        win = -1;
-             
-        int[] lion = new int[info.length];
+        int[] answer = {};
         
-        DFS(info, lion, 0, n);
+        int len = info.length;
+        now = new int[len];
         
-        if(win == -1) {
+        
+        int[] arr = new int[len];
+        DFS(n, info, arr, 0);
+        
+        if(result == 0) {
             return new int[] {-1};
         }
-        return answer;
+        
+        return now;
     }
     
-    public void DFS(int[] info, int[] lion, int cnt, int n) {
-        if(cnt == info.length || n == 0) {
-            cal(info, lion);
+    public void DFS(int arrow, int[] info, int[] arr, int cnt) {
+        if(cnt == info.length || arrow == 0) {
+            cal(info, arr);
             return;
         }
         
-       for(int j=n; j>=0; j--) {
-           lion[cnt] = j; 
-           DFS(info, lion, cnt+1, n-j);
-       }           
+        for(int i=arrow; i>=0; i--) {
+            arr[cnt] = i;
+            DFS(arrow - i, info, arr, cnt+1);
+        }
     }
     
-    public void cal(int[] info, int[] lion) {
-        
-        int a = 0; // 어피치 점수
-        int l = 0; // 라이언 점수
-        
-        // 10 -> 0 반복
+    public void cal(int[] info, int[] arr) {
+        int ap = 0;
+        int lion = 0;
         for(int i=0; i<info.length; i++) {
-            if(info[i] == 0 && lion[i] == 0) {
+            if(info[i] == 0 && arr[i] == 0) {
                 continue;
             }
-            if(info[i] >= lion[i]) {
-                a += 10-i;
+            
+            if(info[i] >= arr[i]) {
+                ap += 10 - i;
             }else {
-                l += 10-i;
+                lion += 10 - i;
             }
         }
         
-        int result = l - a;
+        int value = lion - ap;
         
-        if(result > 0) {
-            if(win < result) {
-                win = result;
-                answer = Arrays.copyOf(lion, lion.length);
-            }else if(win == result) {
-                for(int i=lion.length-1; i>=0; i--) {
-                    if(lion[i] > answer[i]) {
-                        answer = Arrays.copyOf(lion, lion.length);
-                        break;
-                    }
-                    
-                    if(lion[i] == answer[i]) {
-                        continue;
-                    }
+        if(value < 0) {
+            return;
+        }
+        
+        if(result < value) {
+            result = value;
+            now = Arrays.copyOf(arr, arr.length);
+        }else if(result == value) {
+            for(int i=info.length-1; i>=0; i--) {
+                if(arr[i] > now[i]) { 
+                    now = Arrays.copyOf(arr, arr.length);
+                    break;
+                }else if(arr[i] < now[i]) {
                     break;
                 }
-            }         
+            }
         }
     }
 }
