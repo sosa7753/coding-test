936a47a61dce99f94200c218f138f6653de764ac
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-07-16 20:55:43 +0900

    [level 2] Title: 하노이의 탑, Time: 9.40 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,17 +1,14 @@
 import java.util.*;
 class Solution {
     List<int[]> list = new ArrayList<>();
-    public int[][] solution(int n) {            
+    public int[][] solution(int n) {
         hanoi(1, 2, 3, n);
         
         int[][] answer = new int[list.size()][2];
-        for(int i=0; i<list.size(); i++) {
-            int[] now = list.get(i);
-            
-            answer[i][0] = now[0];
-            answer[i][1] = now[1];
+        int idx = 0;
+        for(int[] l : list) {
+            answer[idx++] = l; 
         }
-        
         return answer;
     }
     
@@ -20,8 +17,8 @@ class Solution {
             return;
         }
         
-        hanoi(start, end, mid, cnt-1); // s e m 일때 끝나는 로직
-        list.add(new int[] {start, end});     
-        hanoi(mid, start, end, cnt-1); // m s e 일 때 끝나는 로직 
+        hanoi(start, end, mid, cnt-1);
+        list.add(new int[]{start, end});
+        hanoi(mid, start, end, cnt-1);
     }
 }
