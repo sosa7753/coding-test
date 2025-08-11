562acdb4f80f2c2de9b7d1bde7a9a6ff895eb334
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-12 00:07:08 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 164 ms, Memory: 22072 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -1,42 +1,40 @@
-import java.io.*;
 import java.util.*;
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine().trim();
-        int n = s.length();
-        char[] arr = s.toCharArray();
+        String str = br.readLine();
+        int n = str.length();
+        char[] c = str.toCharArray();
 
         boolean[][] pal = new boolean[n][n];
 
-        // 길이가 1인 구간은 전부 팰린드롬
         for (int i = 0; i < n; i++) {
             pal[i][i] = true;
         }
 
-        // 길이가 2인 구간
         for (int i = 0; i < n - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
+            if (c[i] == c[i + 1]) {
                 pal[i][i + 1] = true;
             }
         }
 
         // 길이가 3 이상인 구간
-        for (int len = 3; len <= n; len++) { // 구간 길이
-            for (int start = 0; start + len - 1 < n; start++) {
-                int end = start + len - 1;
-                if (arr[start] == arr[end] && pal[start + 1][end - 1]) {
-                    pal[start][end] = true;
+        for (int i= 3; i <= n; i++) { // 길이 
+            for (int j = 0; j + i - 1 < n; j++) { // 시작 위치 
+                int end = j + i - 1; // 끝 위치
+                if (c[j] ==  c[end] && pal[j + 1][end - 1]) {
+                    pal[j][end] = true;
                 }
             }
         }
 
         // 최소 분할 DP
         int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, 3000);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 0; i < n; i++) { // 
+            for (int j = 0; j <= i; j++) { // 분할 구간 
                 if (pal[j][i]) {
                     dp[i] = Math.min(dp[i], (j == 0 ? 0 : dp[j - 1]) + 1);
                 }
