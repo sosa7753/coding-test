ee2430dd687adf59763790119b26e5b06794a040
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-14 00:05:03 +0900

    [level 2] Title: 순위 검색, Time: 1148.31 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,75 +1,64 @@
 import java.util.*;
 class Solution {
-    Map<String, List<Integer>> map;
+    Map<String, List<Integer>> map = new HashMap<>();
     public int[] solution(String[] info, String[] query) {
+        
+        for(String s : info) {
+            DFS("", s.split(" "), 0);
+        }
+        
+        for(List<Integer> l : map.values()) {
+            Collections.sort(l);
+        }
+        
         int[] answer = new int[query.length];
-        
-        // 모든 경우의 문자열 조합을 key로 저장
-        map = new HashMap<>();
-        
-        // 모든 문자열과 값 저장
-        for(int i=0; i<info.length; i++) {
-            DFS("", info[i].split(" "), 0);
-        }
-        
-        // 모든 점수 리스트 정렬 
-        for(String key : map.keySet()) {
-            Collections.sort(map.get(key));
-        }
-        
-        // 각 쿼리에 대해 사람 수 구하기
         for(int i=0; i<query.length; i++) {
-            String[] str = query[i].replace(" and ", "").split(" "); // [0] 쿼리 [1] 점수
+            String[] str = query[i].replace(" and ", "").split(" ");
             int score = Integer.parseInt(str[1]);
-            answer[i] = binarySearch(str[0], score);
-        }
-           
+            answer[i] = binarysearch(str[0], score);
+        }   
         return answer;
     }
     
-    public int binarySearch(String str, int score) {
-        if(map.containsKey(str)) {
-            List<Integer> list = map.get(str);
-            
-            int left = 0;
-            int right = list.size()-1;
-            
-            // 점수를 넘은 사람이 없으면 종료 
-            if(list.get(right) < score) {
-                return 0;
-            }
-            
-            // 이진 탐색
-            while(left < right) {
-                int mid = (left + right)/2;
-                
-                int value = list.get(mid);
-                if(value < score) {
-                    left = mid + 1;                 
-                }else {
-                    right = mid;
-                }                      
-            }
-             return list.size() - left;         
-        }
-        return 0;
-    }
-    
-    
-    public void DFS(String str, String[] user, int cnt) {
+    public void DFS(String now, String[] user, int cnt) {
         if(cnt == 4) {
-            if(!map.containsKey(str)) { // 맵에 존재하지 않는 쿼리 
+            if(!map.containsKey(now)) {
                 List<Integer> list = new ArrayList<>();
                 list.add(Integer.parseInt(user[4]));
-                map.put(str, list);
+                map.put(now, list);
             }else {
-                // 객체는 참조를 저장하기 떄문에 다시 넣어줄 필요가 없다.
-                map.get(str).add(Integer.parseInt(user[4])); 
+                map.get(now).add(Integer.parseInt(user[4]));
             }
-            return;
+            return;               
+        }    
+        
+        DFS(now + "-", user, cnt+1);
+        DFS(now + user[cnt], user, cnt+1);
+    }  
+    
+    public int binarysearch(String query, int score) {
+        if(!map.containsKey(query)) {
+            return 0;
+        }
+                 
+        List<Integer> list = map.get(query);
+        int last = list.size()-1;
+        if(list.get(last) < score) {
+            return 0;
         }
         
-        DFS(str+"-", user, cnt+1);
-        DFS(str+user[cnt], user, cnt+1);
+        int l = 0;
+        int r = last;
+        int result = 0;
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(list.get(mid) < score) {
+                l = mid+1;
+            }else {
+                r = mid-1;
+                result = mid;
+            }
+        }
+        return list.size() - result;
     }
-}
+}  
