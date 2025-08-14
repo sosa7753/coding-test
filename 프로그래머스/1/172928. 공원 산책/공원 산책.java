229c25d7f03df28cc8b69604d5a8b26a44b5003d
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-14 17:08:15 +0900

    [level 1] Title: 공원 산책, Time: 0.72 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,88 +1,69 @@
+import java.util.*;
 class Solution {
-    int[] dx = {0, 1, 0, -1};
-    int[] dy = {-1, 0, 1, 0};
+    int[] dr = {-1, 0, 1, 0};
+    int[] dc = {0, 1, 0, -1};
+    int[][] map;
+    Map<String, Integer> direct = new HashMap<>();
+    int r = 0;
+    int c = 0;
     public int[] solution(String[] park, String[] routes) {
-        int p = park.length;
-        int r = routes.length;
-               
-        int[] now = new int[2];
-        
-        // 시작 지점 찾기
-        boolean isStart = false;
-        for(int i=0; i<p; i++) {
+        init();
+        // 공원을 먼저 만들어서 S 찾기 
+        // 명령 수행 메서드 돌리기
+        map = new int[park.length][park[0].length()];     
+        for(int i=0; i<park.length; i++) {
             String s = park[i];
-            
-            String[] str = s.split("");
-            
-            for(int j=0; j<str.length; j++) {
-                if("S".equals(str[j])) {
-                    now[0] = i;
-                    now[1] = j; 
-                    isStart = true;
-                    break;
+            for(int j=0; j<park[i].length(); j++) {
+                if('S' == park[i].charAt(j)) {
+                    r = i; c = j;
+                }else if('X' == park[i].charAt(j)) {
+                    map[i][j] = -1;
                 }
-            }
-            if(isStart) {
-                break;
             }
         }
         
-        // 이동하기
-        for(int i=0; i<routes.length; i++) {
-            String[] str = routes[i].split(" ");
-            int direct = direction(str[0]);
-            int go = Integer.parseInt(str[1]);
+        for(String route : routes) {
+            String[] str = route.split(" ");
+            String dir = str[0];
+            int val = Integer.parseInt(str[1]);
             
-            // 현 위치에서 이동 방향이 맵을 초과하는지 판별
-            int row = now[0] + dy[direct] * go;
-            int col = now[1] + dx[direct] * go;
-            
-            if(row < 0 || row > park.length-1 || col < 0 || col > park[0].length()-1) {
-                continue;
-            }
-            
-            // 현 위치에서 갈 때 장애물이 있을 경우 판별 
-            int cnt = go;
-            int mul = 1;
-            boolean istrue = false;
-            while(cnt > 0) {
-                int row1 = now[0] + dy[direct] * mul;
-                int col1 = now[1] + dx[direct] * mul;
-                
-                String s = park[row1];
-                
-                // 장애물을 만나면 true;
-                if('X' == s.charAt(col1)) {
-                    istrue = true;
-                    break;
-                }
-                cnt--;
-                mul++;
-            }
-            
-            if(istrue) {
-                continue;
-            }
-            
-            now[0] = row;
-            now[1] = col;          
+            move(dir, val);
         }
         
-        return now;
+        int[] answer = new int[2];
+        answer[0] = r;
+        answer[1] = c;
+        return answer;
     }
     
-    // 방향 구하기
-    public int direction(String s) {
-        if("N".equals(s)) {
-            return 0;
-        }else if("E".equals(s)) {
-            return 1;
-        }else if("S".equals(s)) {
-            return 2;
-        }else if("W".equals(s)) {
-            return 3;
-        }else {
-            return 4;
+    public void move(String dir, int val) {
+        int d = direct.get(dir);
+        
+        int nr = r + dr[d]*val;
+        int nc = c + dc[d]*val;
+        if(nr < 0 || nr > map.length-1 || nc < 0 || nc > map[0].length-1) {
+            return;   
         }
-    }  
+        
+        nr = r;
+        nc = c;
+        int cnt = 0;
+        while(cnt < val) {
+            nr += dr[d];
+            nc += dc[d];
+            if(map[nr][nc] == -1) {
+                return;
+            }
+            cnt++;
+        }
+        r = nr;
+        c = nc;
+    }
+    
+    public void init() {
+        direct.put("N", 0);
+        direct.put("E", 1);
+        direct.put("S", 2);
+        direct.put("W", 3);
+    }
 }
