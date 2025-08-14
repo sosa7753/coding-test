2693f9580cf53a040ad3fc8f5ff7bc9957e6db7b
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-14 17:40:06 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.23 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -2,46 +2,33 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         
-        // 알파벳 최초 등장 시점 
+        // Keymap을 Map에 저장해두자. (이 문자는 몇번이 최소)
         Map<Character, Integer> map = new HashMap<>();
-        
-        // Map 에 초기값 저장
-        for(int i=0; i<keymap.length; i++) {    
-            String s = keymap[i];
-            
-            for(int j=0; j<s.length(); j++) {
-                if(map.containsKey(s.charAt(j))) { // 현재 맵에 key가 이미 존재할 때
-                    map.put(s.charAt(j), Math.min(map.get(s.charAt(j)), j+1));
-                    continue;
-                }
-                
-                // 맵에 key가 없을 때 
-                map.put(s.charAt(j), j+1);
-            }
+        for(int i=0; i<keymap.length; i++) {
+           char[] c = keymap[i].toCharArray();
+           for(int j=0; j<c.length; j++) {
+               if(!map.containsKey(c[j])) {
+                   map.put(c[j], j+1);
+               }else {
+                   map.put(c[j], Math.min(map.get(c[j]), j+1));
+               }
+           }
         }
         
         int[] answer = new int[targets.length];
-        int idx = 0;
-        
-        for(int i=0; i<targets.length; i++) { // targets 반복
-            int sum = 0;
-            boolean istrue = false;
-            
-            for(int j=0; j<targets[i].length(); j++) { // 문자열 반복
-                
-               if(!map.containsKey(targets[i].charAt(j))) { // Map에 미 존재 
-                   answer[idx++] = -1;
-                   istrue = true;
-                   break;
-               }
-                
-               sum += map.get(targets[i].charAt(j));
+        for(int i=0; i<targets.length; i++) {
+            char[] c = targets[i].toCharArray();
+            int cnt = 0;
+            for(int j=0; j<c.length; j++) {
+                if(!map.containsKey(c[j])) {
+                    cnt = -1;
+                    break;
+                }else {
+                    cnt += map.get(c[j]);
+                }
             }
-            // 모두 존재 했을 때
-            if(!istrue) {
-                answer[idx++] = sum;
-            }
-        }      
+            answer[i] = cnt;
+        }
         return answer;
     }
 }
