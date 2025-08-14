65468341471d2cb5c3a429fce63556840670f0e9
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-14 17:27:26 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.27 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,38 +1,65 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
-        
         int n = wallpaper.length;
         int m = wallpaper[0].length();
         
-        int rowMin = Integer.MAX_VALUE;
-        int rowMax = 0;
-        int colMin = Integer.MAX_VALUE;
-        int colMax = 0;
-        
-        for(int i=0; i<n; i++) { // 행 
-            String s = wallpaper[i];
+        // 2차원 배열을 순회해서 범위 찾기
+        // 계산하기
+        int[] answer = new int[4]; 
+        boolean[] visited = new boolean[4]; // rmin, rmax, cmin, cmax
+        int rmin = 0;
+        int rmax = 0;
+        int cmin = 0;
+        int cmax = 0;
+        // 행 체크 
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            if(cnt == 2) {
+                break;
+            }
             
-            String[] str = s.split("");
+            String min = wallpaper[i];
+            String max = wallpaper[n -i -1];
+            if(!visited[0] && min.contains("#")) {
+                visited[0] = true;
+                rmin = i;
+                cnt++;
+            }
             
-            for(int j=0; j<str.length; j++) { // 열 
-                if("#".equals(str[j])) {
-                    int row = i;
-                    int col = j;
-                    
-                    rowMin = Math.min(row, rowMin);
-                    rowMax = Math.max(row+1, rowMax);
-                    colMin = Math.min(col, colMin);
-                    colMax = Math.max(col+1, colMax);
-                }
+            if(!visited[1] && max.contains("#")) {
+                visited[1] = true;
+                rmax = n - i;
+                cnt++;
             }
         }
         
-        answer[0] = rowMin;
-        answer[1] = colMin;
-        answer[2] = rowMax;
-        answer[3] = colMax;
+        // 열 체크
+        cnt = 0;
+        for(int i=0; i<m; i++) {
+            if(cnt == 2) {
+                break;
+            }
             
+            for(int j=0; j<n; j++) {
+                if(!visited[2] && wallpaper[j].charAt(i) == '#') {
+                    visited[2] = true;
+                    cmin = i;
+                    cnt++;
+                }
+                
+                if(!visited[3] && wallpaper[j].charAt(m - i - 1) == '#') {
+                    visited[3] = true;
+                    cmax = m - i;
+                    cnt++;
+                }
+            }  
+        }
+        
+        answer[0] = rmin;
+        answer[1] = cmin;
+        answer[2] = rmax;
+        answer[3] = cmax;
+        
         return answer;
     }
 }
