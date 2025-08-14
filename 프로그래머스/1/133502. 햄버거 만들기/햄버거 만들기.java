3028d722db7f2f0b700465b6584da2275456fc3a
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-14 22:34:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 92.07 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,73 +1,35 @@
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
-                
+        Stack<int[]> stack = new Stack<>();
+        
         int answer = 0;
-        
-        Stack<Node> stack = new Stack<>(); // 원본 스택
-        
         for(int i=0; i<ingredient.length; i++) {
+            int now = ingredient[i];
             if(stack.isEmpty()) {
-                if(ingredient[i] == 1) {
-                    stack.push(new Node(1,1));
-                    continue;
-                } 
-                stack.push(new Node(ingredient[i],0));
+                stack.push(new int[]{now, 0});
+                continue;
             }
             
-            if(burger(ingredient[i], stack)) {
-                Node node = stack.peek();
-                stack.push(new Node(ingredient[i], node.cnt + 1));
-                
-                if(ingredient[i] == 1 && node.cnt == 3) {
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    answer++;
-                }
+            if(now == 1) {
+                if(stack.peek()[1] == 2) {
+                   stack.pop();
+                   stack.pop();
+                   stack.pop();
+                   answer++; 
+                }else {
+                    stack.push(new int[]{now, 0});
+                }           
+                continue;
+            }
+            
+            if(now - stack.peek()[0] == 1) {
+                stack.push(new int[]{now, stack.peek()[1]+1});
             }else {
-                if(ingredient[i] == 1) {
-                    stack.push(new Node(1,1));
-                    continue;
-                } 
-                stack.push(new Node(ingredient[i],0));
+                stack.push(new int[]{now, 0});
             }
         }
         
         return answer;
-    }
-    
-    public boolean burger(int value, Stack<Node> stack) {
-        
-        boolean answer = false;
-        // 3가지 경우에 true
-        
-        Node pre = stack.peek();
-        
-        if(value == 2) {
-            if(pre.value == 1 && pre.cnt == 1) {
-                answer = true;
-            }
-        }else if(value == 3) {
-            if(pre.value == 2 && pre.cnt == 2) {
-                answer = true;
-            }
-        }else if(value == 1) {
-            if(pre.value == 3 && pre.cnt == 3) {
-                answer = true;
-            }
-        }       
-        return answer;
-    }
-}
-
-class Node {
-    int value;
-    int cnt;
-    
-    Node(int value, int cnt) {
-        this.value = value;
-        this.cnt = cnt;
     }
 }
