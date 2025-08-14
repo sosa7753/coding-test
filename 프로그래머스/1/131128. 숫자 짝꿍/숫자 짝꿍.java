cf229cccb9df92bf12366cd932497166380d387a
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-14 22:58:15 +0900

    [level 1] Title: 숫자 짝꿍, Time: 182.65 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,60 +1,34 @@
 import java.util.*;
 class Solution {
-    public String solution(String X, String Y) {
+    public String solution(String X, String Y) {       
+        Map<Character, Integer> mapX = new HashMap<>();
+        Map<Character, Integer> mapY = new HashMap<>();
+        char[] cx = X.toCharArray();
+        char[] cy = Y.toCharArray();
         
-        Map<String, Integer> map = new HashMap<>();
-        
-        List<String> list = new ArrayList<>();
-        
-        boolean allZero = true;
-        
-        // X먼저 넣기
-        for(String s : X.split("")) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        for(char c : cx) {
+            mapX.put(c, mapX.getOrDefault(c, 0) + 1);
         }
         
-        // Y로 찾기 
-        for(String s : Y.split("")) {
-            if(!map.containsKey(s)) {
-                continue;
-            }
-            
-            // 존재하는데 0이면, 
-            if(map.get(s) == 0) {
-                continue;
-            }
-            
-            
-            list.add(s);
-            map.put(s, map.get(s)-1);    
-            
-            // 0이 아닐 때 딱 한번 확인 
-            if(!"0".equals(s)) {
-                allZero = false;
-            } 
+        for(char c : cy) {
+            mapY.put(c, mapY.getOrDefault(c, 0) + 1);
         }
         
-        // 내림차순 정렬하기 
-        Collections.sort(list, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for(int i=9; i>=0; i--) {
+            char c = (char)(i + '0');
+            if(!mapX.containsKey(c) || !mapY.containsKey(c)) {
+                continue;
+            }
+            int cnt = Math.min(mapX.get(c), mapY.get(c));
+            sb.append(String.valueOf(c).repeat(cnt));
+        }
         
-        int n = list.size();
-        
-        // 겹치지 않은 경우 
-        if(list.size() == 0) {
+        String str = sb.toString();
+        if("".equals(str)) {
             return "-1";
         }
-        
-        // 0만 존재 
-        if(allZero) {
-            return "0";
-        }
-             
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<n; i++) {
-            sb.append(list.get(i));
-        }
-        String answer = sb.toString();
-        return answer;
+         
+        return str.charAt(0) == '0' ? "0" : str;
     }
 }
