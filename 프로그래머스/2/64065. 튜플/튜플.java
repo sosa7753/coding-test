7aad64d04d3e2a130fda140e3d2143a92f80a3fb
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-20 17:07:37 +0900

    [level 2] Title: 튜플, Time: 53.50 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,16 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        Set<String> set = new HashSet<>();
-        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
-        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
-        int[] answer = new int[arr.length];
-        int idx = 0;
-        for(String s1 : arr) {
-            for(String s2 : s1.split(",")) {
-                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (y[1] - x[1]));
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        int start = 0;
+        int end = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(!Character.isDigit(s.charAt(i))) {
+                if(start != end) {
+                    String str = s.substring(start, end);
+                    map.put(str, map.getOrDefault(str, 0) + 1);
+                }
+                end++;
+                start = end;
+                continue;
             }
+            
+            end++;            
         }
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            pq.offer(new int[] {Integer.parseInt(entry.getKey()), entry.getValue()});
+        }
+        
+        int[] answer = new int[pq.size()];
+        int idx = 0;
+        while(!pq.isEmpty()) {
+            int[] now = pq.poll();
+            answer[idx++] = now[0];
+        }
+     
         return answer;
     }
 }
