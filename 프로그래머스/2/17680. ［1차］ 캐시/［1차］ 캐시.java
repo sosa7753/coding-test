5fcd32e29ceb6f7a5140739f5201b99dddb64840
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-21 17:59:29 +0900

    [level 2] Title: [1차] 캐시, Time: 58.17 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,38 +1,26 @@
 import java.util.*;
-// 리스트에 캐시 넣기 
-// 캐시에 없으면 +5 , 캐시에 추가
-// 있으면 +1  리스트에서 꺼내고 다시 넣기
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        
-        if(cacheSize == 0) {
+        if(cacheSize ==0) {
             return cities.length * 5;
         }
         
-        // 리스트 생성
+        int answer = 0;        
         List<String> list = new ArrayList<>();
-        
-        int answer = 0;
-        
-        for(int i=0; i<cities.length; i++) {
-            String str = cities[i].toUpperCase();
-            
-            // 캐시에 있다면
+        for(String city : cities) {
+            String str = city.toLowerCase();
             if(list.contains(str)) {
-                answer += 1;
+                answer++;
                 list.remove(str);
                 list.add(str);
-                continue;
+            }else {
+                if(list.size() == cacheSize) {
+                    list.remove(0);
+                }    
+                list.add(str);
+                answer += 5;
             }
-            
-            // 캐시에 없다면, 
-            if(list.size() == cacheSize) { // 캐시가 다 찼다. 
-                list.remove(0);
-            }           
-            
-            list.add(str);
-            answer += 5;            
-        }         
+        }
         return answer;
     }
 }
