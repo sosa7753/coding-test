807454ddcf86a23d17955297dd067fc09d4dd9e4
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-21 12:03:58 +0900

    [level 3] Title: 가장 먼 노드, Time: 48.65 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,63 +1,61 @@
 import java.util.*;
 class Solution {
-    int[] dist;
-    List<List<int[]>> list = new ArrayList<>();
+    List<List<Integer>> list = new ArrayList<>();
     public int solution(int n, int[][] edge) {
-        int answer = 0;
         
         for(int i=0; i<=n; i++) {
             list.add(new ArrayList<>());
         }
         
-        for(int i=0; i<edge.length; i++) {
-            list.get(edge[i][0]).add(new int[] {edge[i][1], 1});
-            list.get(edge[i][1]).add(new int[] {edge[i][0], 1});
+        for(int[] ed : edge) {
+            list.get(ed[0]).add(ed[1]);
+            list.get(ed[1]).add(ed[0]);
         }
         
-        Dijkstra(n);
-        
+        int[] dist = dijkstra(n);
+        int answer = 1;
         int max = 0;
-        for(int i=1; i<dist.length; i++) {
-            max = Math.max(dist[i], max);
-        }
-        
-        for(int i=1; i<dist.length; i++) {
+        for(int i=2; i<=n; i++) {
+            if(dist[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            
             if(max == dist[i]) {
                 answer++;
-            }
-        }        
+            }else if(max < dist[i]) {
+                max = dist[i];
+                answer = 1;
+            }      
+        }
         return answer;
     }
     
-    public void Dijkstra(int n) {
+    public int[] dijkstra(int n) {
         boolean[] visited = new boolean[n+1];
-        
-        dist = new int[n+1];
+        int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[1] - y[1]));
-        pq.offer(new int[] {1, 0});
-        
-        dist[1] = 0; 
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[1]-y[1]));
+        pq.offer(new int[]{1, 0});
         
         while(!pq.isEmpty()) {
             int[] now = pq.poll();
+            int to = now[0];
+            int w = now[1];
             
-            if(visited[now[0]]) {
+            if(visited[to]) {
                 continue;
             }
-            visited[now[0]] = true;
             
-            for(int i=0; i<list.get(now[0]).size(); i++) {
-                int[] next = list.get(now[0]).get(i);
-                
-                if(dist[next[0]] > next[1] + now[1]) {
-                    dist[next[0]] = next[1] + now[1];
-                    pq.offer(new int[] {next[0], dist[next[0]]});
+            visited[to] = true;
+            for(int next : list.get(to)) {
+                if(dist[next] > w + 1) {
+                    dist[next] = w + 1;
+                    pq.offer(new int[]{next, dist[next]});
                 }
             }
         }
-        
-        
+        return dist;
     }
 }
