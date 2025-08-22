a2fda02729884da1bc31de933321cdb8ff02c7d7
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-22 20:47:53 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.88 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,30 +1,21 @@
 class Solution {
-    int[][] map;
-    int[][] answer;
-    int min = Integer.MAX_VALUE;
+    int[][] b;
+    int[][] t;
+    int answer = Integer.MAX_VALUE;
     public int solution(int[][] beginning, int[][] target) {
-        map = beginning;
-        answer = target;
+        b = beginning;
+        t = target;
         
         DFS(0, 0);
-    
-        if(min == Integer.MAX_VALUE) {
-            return -1;
-        }
-        return min;
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
     
     public void DFS(int row, int cnt) {
-        if(row == map.length) {
-            for(int i=0; i<map[0].length; i++) {
-                int result = check(i);
-                if(result == -1) {
-                    return;
-                }
-                cnt += result;
+        if(row == b.length) {
+            int r = check();
+            if(r != -1) {
+                answer = Math.min(answer, cnt + r);
             }
-            
-            min = Math.min(min, cnt);
             return;
         }
         
@@ -34,26 +25,29 @@ class Solution {
         DFS(row+1, cnt);
     }
     
-    public int check(int col) {
-        int cnt = 0;
-        for(int i=0; i<map.length; i++) {
-            if(map[i][col] == answer[i][col]) {
-                cnt++;
+    public int check() {
+        int result = 0;
+        for(int i=0; i<b[0].length; i++) { // 열 체크 
+            int cnt = 0;
+            for(int j=0; j<b.length; j++) {
+                if(b[j][i] == t[j][i]) {
+                    cnt++;
+                }
+            }
+            if(cnt == 0) {
+               result++; 
+            }else if(cnt == b.length) {
+               continue;
+            }else {
+                return -1;
             }
         }
-        
-        if(cnt == map.length) {
-            return 0;
-        }else if(cnt == 0) {
-            return 1;
-        }else {
-            return -1;
-        }
+        return result;
     }
     
     public void filp(int row) {
-        for(int i=0; i<map[0].length; i++) {
-            map[row][i] = (map[row][i] + 1)%2;
+        for(int i=0; i<b[0].length; i++) {
+            b[row][i] = (b[row][i] + 1)%2;
         }
     }
 }
