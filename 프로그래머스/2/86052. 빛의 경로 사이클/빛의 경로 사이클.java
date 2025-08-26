56b7325d7b2b2de69364504112e5d5549b909135
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-26 11:45:15 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 146.61 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
@@ -1,78 +1,82 @@
 import java.util.*;
 class Solution {
+    int[] dr = {-1, 0, 1, 0};
+    int[] dc = {0, 1, 0, -1};
+    int n,m;
+    char[][] map;
     boolean[][][] visited;
-    int[] dx = {0, 1, 0, -1};
-    int[] dy = {-1, 0, 1, 0};
-    int n;
-    int m;
-    List<Integer> list = new ArrayList<>();
-    public int[] solution(String[] grid) {        
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public int[] solution(String[] grid) {
         n = grid.length;
         m = grid[0].length();
+        map = new char[n][m];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                map[i][j] = grid[i].charAt(j);
+            }
+        }
         
         visited = new boolean[n][m][4];
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                // 0 : 상 1 : 우 2 : 하 3 : 좌 
                 for(int k=0; k<4; k++) {
                     if(visited[i][j][k]) {
                         continue;
-                    }      
-                    list.add(BFS(grid, i,j,k));
+                    }
+                    pq.offer(razer(i, j, k));
                 }
             }
         }
-        
-        Collections.sort(list);
-        int[] answer = new int[list.size()];
+        int[] answer = new int[pq.size()];
         int idx = 0;
-        for(int i=0; i<answer.length; i++) {
-            answer[idx++] = list.get(i);
-        } 
-        
+        while(!pq.isEmpty()) {
+            answer[idx++] = pq.poll();
+        }
         return answer;
     }
     
-    public int BFS(String[] grid, int row, int col, int direct) {
+    public int razer(int row, int col, int dir) {
         int result = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{row,col,dir});
         
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {row, col, direct});
-                
-        while(!queue.isEmpty()) {
-            int[] start = queue.poll();
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            int r = now[0]; int c = now[1]; int d = now[2];
             
-            if(visited[start[0]][start[1]][start[2]]) {
+            if(visited[r][c][d]) {
                 continue;
             }
-            visited[start[0]][start[1]][start[2]] = true;
+            visited[r][c][d] = true;
+           
+            int nr = now[0] + dr[d];
+            int nc = now[1] + dc[d];
             
-            int r = start[0] + dy[start[2]]; 
-            int c = start[1] + dx[start[2]];
-            
-            // 좌표 체크 
-            if(r < 0 || r > n-1) {
-                r = Math.abs(n-1 - start[0]);
-            }
-        
-            if(c < 0 || c > m-1) {
-                c = Math.abs(m-1 - start[1]);
+            if(nr < 0 || nr > n-1) {
+                nr = Math.abs(n-1 - now[0]);
             }
             
-            // 방향 잡기
-            char next = grid[r].charAt(c);
-            int nextK = start[2];
-            if(next == 'L') { // 0 -> 3,  1 -> 0, 2 -> 1, 3 -> 2
-                nextK = (nextK + 3)%4;
-            }else if(next == 'R') { // 0 -> 1,  1 -> 2, 2 -> 3, 3 -> 0
-                nextK = (nextK + 1)%4;
+            if(nc < 0 || nc > m-1) {
+                nc = Math.abs(m-1 - now[1]);
             }
             
-            queue.offer(new int[] {r, c, nextK});
-            result++;         
+            int nd = nextDir(map[nr][nc], d);
+            
+            q.offer(new int[]{nr, nc, nd});
+            result++;
         }
         
-        return result;    
+        return result;     
+    }
+    
+    public int nextDir(char c, int d) {
+        if(c == 'S') {
+            return d;
+        }else if(c == 'L') {
+            return (d - 1 + 4)%4;
+        }else {
+            return (d + 1)%4;
+        }      
     }
 }
