76389df59dc78e7d149db4299fe7120cc08e6dca
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-08-26 10:49:43 +0900

    [level 3] Title: 섬 연결하기, Time: 0.68 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,46 +1,45 @@
 import java.util.*;
 class Solution {
-    List<List<int[]>> list = new ArrayList<>();
-    boolean[] visited;
+    int[] parents;
     public int solution(int n, int[][] costs) {
-        int answer = 0;
-        
-        for(int i=0; i<=n; i++) {
-            list.add(new ArrayList<>());
-        }
-        
-        for(int i=0; i<costs.length; i++) {
-            list.get(costs[i][0]).add(new int[] {costs[i][1], costs[i][2]});
-            list.get(costs[i][1]).add(new int[] {costs[i][0], costs[i][2]});
-        }
-        return prim(n, costs);
+      return kruskal(n, costs);        
     }
     
-    public int prim(int n, int[][] costs) {
+    public int kruskal(int n, int[][] costs) {
         int result = 0;
-        visited = new boolean[n+1];
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[1] - y[1]));
-        pq.offer(new int[] {1, 0});
+        parents = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            parents[i] = i;
+        }
         
-        while(!pq.isEmpty()) {
-            int[] now = pq.poll();
-            
-            if(visited[now[0]]) {
-                continue;
-            }
-            visited[now[0]] = true;
-            result += now[1];
-            
-            for(int i=0; i<list.get(now[0]).size(); i++) {
-                int[] next = list.get(now[0]).get(i);
-                
-                if(visited[next[0]]) {
-                    continue;
-                }                
-                pq.offer(next);               
+        Arrays.sort(costs, (x,y) -> (x[2] - y[2]));
+        
+        for(int[] cost : costs) {
+            if(find(cost[0]) != find(cost[1])) {
+                union(cost[0], cost[1]);
+                result += cost[2];
             }
         }
         return result;
     }
- }
+    
+    public void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if(a <= b) {
+            parents[b] = a;
+        }else {
+            parents[a] = b;
+        }
+    }
+    
+    public int find(int a) {
+        if(a == parents[a]) {
+            return a;
+        }
+        
+        return parents[a] = find(parents[a]);
+    }
+}
