575d9ddbac5458cfa84469289f41752e3f55771a
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-09-10 13:33:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.64 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,28 @@
 class Solution {
+    long MOD = 1234567;
+    long[] dp;
     public long solution(int n) {
-        long answer = 0;
-        
-        if(n==1) {
+        if(n == 1) {
             return 1;
-        }else if(n==2) {
-            return 2;
         }
-        
-        long[] dp = new long[n+1];
+        dp = new long[n+1];
         dp[1] = 1;
         dp[2] = 2;
-        
-        for(int i=3; i<=n; i++) {
-            dp[i] += (dp[i-1]%1234567 + dp[i-2]%1234567)%1234567;          
+        return solve(n);
+    }
+    
+    public long solve(int now) {
+        if(dp[now] !=0){
+            return dp[now];
         }
-              
-        return dp[n];
+                
+        if(now - 1 >= 0) {
+            dp[now] += solve(now-1);
+        }
+        
+        if(now - 2 >= 0) {
+            dp[now] += solve(now-2);
+        }
+        return dp[now] = dp[now]%MOD;
     }
 }
