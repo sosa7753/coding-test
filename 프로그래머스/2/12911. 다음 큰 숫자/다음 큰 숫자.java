075a439c5e88285d5201e48223de69fa5f108dfa
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-09-10 16:57:39 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,31 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        int n1 = binary(n);
-        int number = n+1;
-        while(true) {
-            if(binary(number) == n1) {
-                answer = number;
-                break;
-            }
-            number++;
-        }
-        return answer;
-    }
-    
-    public int binary(int num) {
-        int tmp = num;
-        
-        int cnt = 0;
-        
-        while(num !=1) {
-            if(num%2 == 1) {
-               cnt++; 
-            }
-            
-            num = num/2;
-        }
-        return cnt+1;
+        long x = n;
+        long c = x & -x;
+        long r = x + c;
+        long ones = ((x ^ r) >> 2) / c;
+        return (int)(r | ones);
+
     }
 }
