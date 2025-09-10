a39cfa07e742b5d45c943ca3faabdc6014324ae1
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-09-10 11:21:12 +0900

    [level 2] Title: 숫자 변환하기, Time: 25.67 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,37 +1,27 @@
 class Solution {
-    public int solution(int x, int y, int n) {
-        int answer = 0;
-        
-        int[] dp = new int[y+1];
-        for(int i=x; i<=y; i++) {
+    public int solution(int x, int y, int n) {      
+        int[] dp = new int[y + 1];
+        for(int i=x+1; i<=y; i++) {
             dp[i] = Integer.MAX_VALUE;
         }
-        
-        dp[x] = 0;
-        
         for(int i=x; i<=y; i++) {
             if(dp[i] == Integer.MAX_VALUE) {
                 continue;
             }
-            // n 더하기
-            if(i+n <= y) {
+            
+            if(i + n <= y){
                 dp[i+n] = Math.min(dp[i+n], dp[i] + 1);
             }
             
-            // x2 
             if(i*2 <= y) {
-                dp[2*i] = Math.min(dp[2*i], dp[i] + 1);
+                dp[i*2] = Math.min(dp[i*2], dp[i] +1);
             }
             
-            // x3 
-            if(i*3 <=y) {
-                dp[3*i] = Math.min(dp[3*i], dp[i] + 1);
+            if(i*3 <= y) {
+                dp[i*3] = Math.min(dp[i*3], dp[i] + 1);
             }
         }
-              
-        if(dp[y] == Integer.MAX_VALUE) {
-            return -1;
-        }
-        return dp[y];
+        
+        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
     }
 }
