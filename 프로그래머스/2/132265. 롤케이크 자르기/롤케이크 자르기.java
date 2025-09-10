33c4c0d044db44a9385edd2ddff358f18cdd09af
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-09-10 16:35:23 +0900

    [level 2] Title: 롤케이크 자르기, Time: 260.97 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,34 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
+        
+        Map<Integer, Integer> b = new HashMap<>();
+        for(int top : topping) {
+            b.put(top, b.getOrDefault(top, 0) + 1);
+        }
+        
         int answer = 0;
         
-        // 각 토핑 종류 개수 세주기 
-        Map<Integer, Integer> two = new HashMap<>();
-        for(int i=0; i<topping.length; i++) {
-            two.put(topping[i], two.getOrDefault(topping[i], 0) + 1);
-        }
-                
-        int p = 0;
-        
-        Map<Integer, Integer> one = new HashMap<>();
-        while(p <= topping.length-2) {
-            one.put(topping[p], one.getOrDefault(topping[p], 0) + 1);
+        Map<Integer, Integer> a = new HashMap<>();    
+        for(int top : topping) {
+            a.put(top, a.getOrDefault(top, 0) + 1);
             
-            if(two.containsKey(topping[p])) {
-                if(two.get(topping[p]) == 1) {
-                    two.remove(topping[p]);
-                }else {
-                    two.put(topping[p], two.get(topping[p]) -1);
-                }
+            int v = b.get(top);
+            if(v == 1) {
+                b.remove(top);        
+            }else {
+                b.put(top, v-1);
             }
             
-            p++;
-            
-            if(one.size() == two.size()) {
+            if(a.size() == b.size()) {
                 answer++;
+            }else if(a.size() > b.size()) {
+                break;
             }
-        }   
+        }
+        
         return answer;
     }
 }
