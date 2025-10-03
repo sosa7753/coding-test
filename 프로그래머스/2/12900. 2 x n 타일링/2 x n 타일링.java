7877ca233912f7b9203139737254416bcfcb519f
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-03 23:28:55 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.95 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,12 +1,14 @@
 class Solution {
+    long INF = 1_000_000_007L;
     public int solution(int n) {
-       
-        int[] dp = new int[n+1];
+        long[] dp = new long[n+1];
         dp[1] = 1;
         dp[2] = 2;
+        
         for(int i=3; i<=n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+            dp[i] = (dp[i-1] + dp[i-2])%INF;
         }
-        return dp[n];
+        
+        return (int)dp[n];
     }
 }
