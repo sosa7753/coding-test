9b94562227faccfa12e39edf90f434821869e2f0
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-06 00:39:30 +0900

    [level 2] Title: 괄호 변환, Time: 2.57 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,81 +1,66 @@
 import java.util.*;
 class Solution {
     StringBuilder sb = new StringBuilder();
-    // 짝 개수가 같음 -> 균형잡힌 문자열
-    // 짝이 모두 맞음 -> 올바른 괄호 문자열
     public String solution(String p) {
-             
-        if(check(p)) {
-            return p;
-        }   
-        
         DFS(p);
-        
         return sb.toString();
     }
     
-    
-    public void DFS(String start) {
-        if("".equals(start)) {
+    public void DFS(String now) {
+        if(check(now)) {
+            sb.append(now);
             return;
         }
         
-        int o = 0;
-        int c = 0;
-        int p = 0;
-        while(true) {
-            char sc = start.charAt(p);
-            if(sc == '(') {
-                o++;
-            }else {
-                c++;
-            }
-            
-            if(o == c) {
-                // 올바른 괄호 문자열 
-                String u = start.substring(0, p+1);
-                String v = start.substring(p+1);
-                if(check(u)) {
-                    sb.append(u);
-                    DFS(v);
-                    break;
-                }
-                
-                // 올바르지 않은 문자열
-                sb.append("(");
-                DFS(v);
-                sb.append(")");
-                sb.append(change(u));
-                break;
-            }
-            p++;
-        }
-    }
-    // 올바른 괄호 문자열 체크
-    public boolean check(String p) {
-        Stack<Character> stack = new Stack<>();
+        int idx = div(now);
         
-        for(int i=0; i<p.length(); i++) {
-            char c = p.charAt(i);
-            
-            // 비어 있을 때
-            if(stack.isEmpty()) {
-                if(c == ')') {
-                    return false;
-                }else {
-                    stack.push(c);
-                }
-                continue;
+        String u = now.substring(0, idx+1);
+        String v = now.substring(idx+1);
+        
+        if(check(u)) {
+            sb.append(u);
+            DFS(v);
+        }else {
+            sb.append("(");
+            DFS(v);
+            sb.append(")");
+            sb.append(change(u));
+        }             
+    }
+    
+    public int div(String s) {
+        char[] c = s.toCharArray();
+        
+        int l = 0;
+        int r = 0;
+        for(int i=0; i<c.length; i++) {
+            if(c[i] == '(') {
+                l++;
+            }else {
+                r++;
             }
             
-            // 비어 있지 않을 때 
-            if('(' == c) {
-                stack.push(c);
+            if(l == r) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public boolean check(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] c = s.toCharArray();
+        for(char cc : c) {
+            if(cc == '(') {
+                stack.push(cc);
             }else {
-                if(stack.peek() == ')') {
+                if(stack.isEmpty()) {
                     return false;
+                }else if(stack.peek() == '(') {
+                    stack.pop();
+                }else {
+                    stack.push(cc);
                 }
-                stack.pop();
             }
         }
         
@@ -84,18 +69,16 @@ class Solution {
         }
         return false;
     }
-      
-    public String change(String s) {
-        if(s.length() <=2) {
-            return "";
-        }
     
+    public String change(String s) {
+        s = s.substring(1, s.length()-1);
+        
         s = s.replace("(", "1");
         s = s.replace(")", "2");
         
         s = s.replace("1", ")");
         s = s.replace("2", "(");
-                 
-        return s.substring(1,s.length()-1);
+        
+        return s;
     }
 }
