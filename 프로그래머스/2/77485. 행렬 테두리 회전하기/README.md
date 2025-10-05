9e4e768dc972737f03469a44180bed3d37f74f80
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-05 15:14:30 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 14.44 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/README.md
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬 테두리 회전하기 - 77485 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77485?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77485) 
 
 ### 성능 요약
 
-메모리: 86 MB, 시간: 17.72 ms
+메모리: 83.5 MB, 시간: 14.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 27일 10:09:29
+2025년 10월 05일 15:14:28
 
 ### 문제 설명
 
--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,58 +1,57 @@
 import java.util.*;
 class Solution {
-    int[] dx = {1, 0, -1, 0};
-    int[] dy = {0, 1, 0, -1};      
     int[][] map;
-    int[] answer;
-    int idx;
     public int[] solution(int rows, int columns, int[][] queries) {
-        answer = new int[queries.length];        
-        idx = 0;   
-        
         map = new int[rows][columns];
-        int cnt = 1;
-        for(int i=0; i<map.length; i++) {
-            for(int j=0; j<map[i].length; j++) {
-                map[i][j] = cnt++;
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<columns; j++) {
+                map[i][j] = i * columns + j + 1;
             }
         }
-        
-        // (2,2) ~(5,4) 회전
-        // 1~4 행, 1~3열 변경, 
-        
-        for(int i=0; i<queries.length; i++) {
-            int gap = (queries[i][2] - queries[i][0] + queries[i][3] - queries[i][1]) * 2;
-            
-            rotate(queries[i][0]-1, queries[i][1]-1, queries[i][0]-1, queries[i][1]-1,
-                   queries[i][2]-1, queries[i][3]-1, 0, 
-                   map[queries[i][0]-1][queries[i][1]-1], gap, Integer.MAX_VALUE, 0);
+              
+        int n = queries.length;
+        int[] answer = new int[n];      
+        for(int i=0; i<n; i++) {
+            answer[i] = 
+                rotate(queries[i][0]-1, queries[i][1]-1, queries[i][2]-1, queries[i][3]-1);
         }
-        
         return answer;
     }
     
-    public void rotate(int row, int col, int rs, int cs, int row2, int col2, int direct, 
-                       int pre, int max, int min, int depth) {
-        if(depth == max+1) {
-            answer[idx++] = min;
-            return;
+    public int rotate(int rs, int cs, int re, int ce) {
+        // tmp 값을 나한테 적고, 내 값은 pre에 넣기 -> pre = tmp
+        int min = map[rs][cs];
+        int tmp = map[rs][cs];  
+        int pre = 0;  
+        for(int i=cs+1; i<=ce; i++) { // 위
+            pre = map[rs][i]; 
+            map[rs][i] = tmp; 
+            tmp = pre;        
+            min = Math.min(min, tmp);
         }
         
-        int now = map[row][col]; 
-        map[row][col] = pre;        
-        
-        int r = row + dy[direct%4];
-        int c = col + dx[direct%4];
-        int nextDirect = direct;
-        
-        if(r < rs || r > row2 || c < cs || c > col2) {
-            nextDirect++;
-            r = row + dy[nextDirect%4];
-            c = col + dx[nextDirect%4];
+        for(int i=rs+1; i<=re; i++) { // 우
+            pre = map[i][ce];
+            map[i][ce] = tmp;
+            tmp = pre;
+            min = Math.min(min, tmp);
         }
         
-        min = Math.min(min, now);
-  
-        rotate(r, c, rs, cs, row2, col2, nextDirect, now, max, min, depth+1);        
+        for(int i=ce-1; i>=cs; i--) { // 하
+            pre = map[re][i];
+            map[re][i] = tmp;  
+            tmp = pre;
+            min = Math.min(min, tmp);
+        }
+        
+
+        for(int i=re-1; i>=rs; i--) { // 좌
+            pre = map[i][cs];
+            map[i][cs] = tmp;
+            tmp = pre;
+            min = Math.min(min, tmp);
+        }
+        
+        return min;
     }
 }
