efdf6d9a933b732af280adb0f6b060b445f4ea0c
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-08 00:22:14 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,23 +2,21 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         int len = name.length();
-        int move = name.length()-1; // 최소 좌우 이동 횟수
+        int move = name.length() -1;
         
-        
-        for(int i=0; i<len; i++) {
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+        for(int i=0; i<len; i++) {           
+            answer += Math.min(name.charAt(i) - 'A' , 'Z' - name.charAt(i) + 1);
             
-            // 연속된 'A'가 끝나는 지점 찾기
-            int next = i+1;
+            int next = i+1;          
             while(next < len && name.charAt(next) == 'A') {
                 next++;
             }
             
-            move = Math.min(move, (i*2) + len - next); // 0 -> i -> 0 -> 끝지점
-            move = Math.min(move, (len - next) * 2 + i); // 0 -> 끝지점 -> 0 -> i            
+            move = Math.min(move, (i*2) + len - next);
+            move = Math.min(move, (len- next)*2 + i);              
         }
-        answer += move;
         
-        return answer; 
+        answer += move;
+        return answer;
     }
 }
