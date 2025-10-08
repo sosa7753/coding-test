651846be3cbacab3c4f6586eb1e8c6f7c0c220b3
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-08 22:56:34 +0900

    [level 2] Title: 당구 연습, Time: 0.64 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -1,46 +1,43 @@
 class Solution {
     public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
-        int[] answer = new int[balls.length];
-        int idx = 0;
+        int b = balls.length;
         
-        // (0,5) (5, n), (5, 0), (m,5)
-        for(int i=0; i<balls.length; i++) {
+        int[] answer = new int[b];
+        for(int i=0; i<b; i++) {
             int min = Integer.MAX_VALUE;
             
-            // x = 0
+            // x == 0
             if(startY != balls[i][1] || startX < balls[i][0]) {
-                double d1 = Math.pow((double)(balls[i][0] + startX), 2) + 
-                            Math.pow((double)(balls[i][1] - startY), 2);
+                double d = Math.pow((double)(balls[i][0] + startX), 2) + 
+                           Math.pow((double)(balls[i][1] - startY), 2);
                 
-                min = Math.min((int)d1, min);
+                min = Math.min(min, (int)d);
             }
             
-            
-            // x = m
+            // x == m
             if(startY != balls[i][1] || startX > balls[i][0]) {
-                double d2 = Math.pow((double)(2*m - balls[i][0] - startX) , 2) + 
-                            Math.pow((double)(balls[i][1] - startY), 2);
+               double d = Math.pow((double)(2*m - startX - balls[i][0]), 2) + 
+                          Math.pow((double)(balls[i][1] - startY), 2);  
                 
-                min = Math.min((int)d2, min);
+               min = Math.min(min, (int)d);
             }
             
-            // y = 0;
+            // y = 0 
             if(startX != balls[i][0] || startY < balls[i][1]) {
-                double d3 = Math.pow((double)(balls[i][0] - startX), 2) + 
-                            Math.pow((double)(balls[i][1] + startY), 2);
+               double d = Math.pow((double)(balls[i][1] + startY), 2) + 
+                          Math.pow((double)(balls[i][0] - startX), 2);
                 
-                min = Math.min((int)d3, min);
+               min = Math.min(min, (int)d);
             }
             
-            // y = n;
+            // y = n
             if(startX != balls[i][0] || startY > balls[i][1]) {
-                double d4 = Math.pow((double)(balls[i][0] - startX), 2) + 
-                            Math.pow((double)(2*n - balls[i][1] - startY), 2);
-                
-                min = Math.min((int)d4, min);
-            }
+               double d = Math.pow((double)(2*n - balls[i][1] - startY), 2) + 
+                          Math.pow((double)(balls[i][0] - startX), 2); 
             
-            answer[idx++] = min;
+               min = Math.min(min, (int)d);
+            } 
+            answer[i] = min;
         }
         
         return answer;
