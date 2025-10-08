c4e4b04398f97281a4451573c7e76fbc92a30113
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-08 20:57:25 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 449.97 ms, Memory: 325 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,35 +1,30 @@
 import java.util.*;
 class Solution {
+    Map<Integer, Integer> in = new HashMap<>(); // 나한테 들어오는 간선
+    Map<Integer, Integer> out = new HashMap<>(); // 내가 보내는 간선
     public int[] solution(int[][] edges) {
-        Map<Integer, Integer> out = new HashMap<>();
-        Map<Integer, Integer> in = new HashMap<>();
-        int[] answer = new int[4];
-        
-        // 간선 정리 
-        for(int i=0; i<edges.length; i++) {
-            out.put(edges[i][0], out.getOrDefault(edges[i][0], 0) + 1);
-            in.put(edges[i][1], in.getOrDefault(edges[i][1], 0) + 1);
+        for(int[] e : edges) {
+            out.put(e[0], out.getOrDefault(e[0], 0) + 1);
+            in.put(e[1], in.getOrDefault(e[1], 0) + 1);
         }
         
-        // 정점 찾기
-        for(int value : out.keySet()) {
-            if(out.get(value) > 1) {
-                if(!in.containsKey(value)) {
-                    answer[0] = value; // 만족하면 정점.
+        int[] answer = new int[4];
+        for(int k : out.keySet()) {
+            if(out.get(k) > 1) {
+                if(!in.containsKey(k)) {
+                    answer[0] = k;
                 }else {
-                    answer[3]++; // 만족하지 않으면 8자 그래프
+                    answer[3]++; // 8자
                 }
             }
         }
         
-        // 막대 그래프
-        for(int value : in.keySet()) {
-            if(!out.containsKey(value)) { // 나가는게 없다면 막대 그래프 
+        for(int k : in.keySet()) {
+            if(!out.containsKey(k)) {
                 answer[2]++;
-            }   
+            }
         }
         
-        // 도넛 그래프 개수 = 정점에서의 간선 수 - 막대 수 - 8자 수 
         answer[1] = out.get(answer[0]) - answer[2] - answer[3];
         return answer;
     }
