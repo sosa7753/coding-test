f616bcf7833cca6827c3358fbc50c475c3568444
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-09 22:48:50 +0900

    [level 2] Title: 모음 사전, Time: 0.20 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,14 +1,16 @@
+import java.util.*;
 class Solution {
     public int solution(String word) {
-        int answer = 0;
+        int answer = 0; 
+        // 전체 문자열 5^5 + 5^4 + 5^3 + 5^2 + 5^1 = 3125 + 625 + 125 + 25 + 5 = 3905
         int total = 3905;
+        
         String str = "AEIOU";
         
         String[] arr = word.split("");
-        
         for(String s : arr) {
             total /= 5;
-            answer += total*str.indexOf(s) + 1;
+            answer += total * str.indexOf(s) + 1; 
         }
         return answer;
     }
