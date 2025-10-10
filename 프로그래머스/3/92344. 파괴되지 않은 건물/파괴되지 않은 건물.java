db67f4af5c5c82c787c8a2072b81f9f7c03235bd
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-10 18:46:56 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 59.87 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,44 +1,40 @@
 class Solution {
+    int[][] map;
     public int solution(int[][] board, int[][] skill) {
-        int answer = 0;
+        int n = board.length;
+        int m = board[0].length;
         
-        int[][] S = new int[board.length+1][board[0].length+1];
-        for(int i=0; i<skill.length; i++) {
-            int power = skill[i][5];
-            if(skill[i][0] == 1) {
-                power = -power;
-            }
+        map = new int[n+1][m+1];
+        for(int[] s : skill) {
+            int power = (s[0] == 1) ? -s[5] : s[5]; 
             
-            // 더하는 시작에 power, 더하는 끝에 -power
-            S[skill[i][1]][skill[i][2]] += power;
-            S[skill[i][1]][skill[i][4]+1] += -power;
-            S[skill[i][3]+1][skill[i][2]] += -power;
-            S[skill[i][3]+1][skill[i][4]+1] += power;                        
+            int r1 = s[1]; int c1 = s[2]; int r2 = s[3]; int c2 = s[4];
+            map[r1][c1] += power;
+            map[r1][c2+1] -= power;
+            map[r2+1][c1] -= power;
+            map[r2+1][c2+1] += power;
         }
-              
-        // 누적합 당겨오기 
-        // 상하 합산
-        for(int i=1; i<S.length; i++) {
-            for(int j=0; j<S[0].length; j++) {
-                S[i][j] += S[i-1][j];
+        
+        for(int i=1; i<n; i++) { // 상하 합산 
+            for(int j=0; j<m; j++) {
+                map[i][j] += map[i-1][j];
             }
         }
         
-        // 좌우 합산 
-        for(int i=1; i<S[0].length; i++) {
-            for(int j=0; j<S.length; j++) {
-                S[j][i] += S[j][i-1];
+        for(int i=1; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                map[j][i] += map[j][i-1];
             }
         }
         
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[0].length; j++) {
-                if(board[i][j] + S[i][j] > 0) {
+        int answer = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(map[i][j] + board[i][j] > 0) {
                     answer++;
                 }
             }
         }
-                
         return answer;
     }
 }
