7c345d29ed463dc4eb99edd16b6448f5fa4b62d0
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-10 17:40:23 +0900

    [level 3] Title: 거스름돈, Time: 48.92 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,16 +1,15 @@
 class Solution {
+    long INF = 1_000_000_007L;
     public int solution(int n, int[] money) {
-        int[] dp = new int[n+1];
-        
+        long[] dp = new long[n+1];       
         for(int i=0; i<money.length; i++) {
-            dp[money[i]] += 1; 
-            for(int j=0; j<=n; j++) {
+            dp[money[i]] += 1;
+            for(int j=1; j<=n; j++) {
                 if(j >= money[i]) {
-                    dp[j] = (dp[j] + dp[j-money[i]])%1000000007;
+                    dp[j] = (dp[j] + dp[j-money[i]])%INF;
                 }
             }
         }
-        
-        return dp[n];
+        return (int)(dp[n]);
     }
 }
