5ec3e44369081a80b27bcbc9d18a0f16b516b15b
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-10 11:35:47 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 17.27 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,18 +1,19 @@
 import java.util.*;
 class Solution {
-    public String solution(int n, int t, int m, String[] timetable) {
+    public String solution(int n, int t, int m, String[] timetable) {       
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        int start = time("09:00");            
-        for(int i=0; i<timetable.length; i++) {
-            pq.offer(time(timetable[i]));
+        for(String u : timetable) {
+            pq.offer(time(u));
         }
         
-        int cnt = 0;
+        int start = time("09:00");
         int last = pq.peek();
         int lastBus = start + (n-1) * t;
+        
+        int cnt = 0;
         for(int i=0; i<n; i++) {
-            int now = start + i * t; // 버스 시간
+            int now = start + i * t;
             cnt = 0;
             
             while(!pq.isEmpty()) {
@@ -20,37 +21,22 @@ class Solution {
                     break;
                 }
                 last = pq.poll();
-                cnt++;               
-            }    
+                cnt++;
+            }
         }
         
-        if(cnt < m) {
+        if(cnt < m) { // 마지막 버스가 미달
             return clock(lastBus);
         }
         return clock(last - 1);
     }
-       
-    public int time(String str) {
-        String[] s = str.split(":");
-        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+    
+    public int time(String clock) {
+        String[] str = clock.split(":");
+        return Integer.parseInt(str[0]) * 60  + Integer.parseInt(str[1]);
     }
     
     public String clock(int time) {
-        String h = String.valueOf(time/60);
-        String m = String.valueOf(time%60);
-        
-        StringBuilder sb = new StringBuilder();
-        
-        if(h.length() == 1) {
-            sb.append("0");
-        }
-        sb.append(h);
-        sb.append(":");
-        if(m.length() == 1) {
-            sb.append("0");
-        }
-        sb.append(m);
-        
-        return sb.toString();
+        return String.format("%02d:%02d", time/60, time%60);
     }
 }
