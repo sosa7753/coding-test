bad1d6ec66267f543bb7096028bcdfffa18a7161
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-10 20:12:50 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 15.59 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,21 +1,24 @@
 class Solution {
     int answer = 0;
     public int solution(int n) {       
+        // ( 14개 사용 가능 
+        // ) 14개 사용 가능 
+        // DFS로 만들어서 세자 백트래킹 주의
         DFS(0, 0, 0, n);
         return answer;
     }
     
-    public void DFS(int cnt, int plus, int minus, int n) {
-        if(plus == n || cnt == 2*n) {
+    public void DFS(int cnt, int open, int close, int n) {
+        if(open == n || cnt == 2 * n) {
             answer++;
             return;
         }
         
-        if(plus < minus) {
+        if(close > open) {
             return;
         }
         
-        DFS(cnt+1, plus+1, minus, n);
-        DFS(cnt+1, plus, minus+1, n);       
+        DFS(cnt+1, open+1, close, n);
+        DFS(cnt+1, open, close+1, n);
     }
 }
