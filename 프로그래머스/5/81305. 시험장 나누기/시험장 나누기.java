61b2a440b85de0984960d4d8e812d51bee0ad1a8
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-10 17:10:00 +0900

    [level 5] Title: 시험장 나누기, Time: 18.39 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
+++ 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
@@ -1,25 +1,23 @@
+import java.util.*;
 class Solution {
-    int size = 0;
+    int n, size, answer;
     int root = -1;
-    int n,k;
     int[] num;
     int[][] links;
     public int solution(int k, int[] num, int[][] links) {
-        this.k = k;
-        this.num = num;
-        this.links = links; 
-        
-        // root가 누군지 찾기 -> 누군가의 자식이면 visited = true로 찾기
-        // 명수를 이진탐색으로 돌려서 k개의 그룹으로 나눠지는지 체크
         n = num.length;
+        this.num = num;
+        this.links = links;
+
+        // 루트 찾기
         boolean[] visited = new boolean[n];
-        for(int[] link : links) {
-            if(link[0] != -1) {
-                visited[link[0]] = true;
+        for(int i=0; i<n; i++) {
+            if(links[i][0] != -1) {
+                visited[links[i][0]] = true;
             }
             
-            if(link[1] != -1) {
-                visited[link[1]] = true;
+            if(links[i][1] != -1) {
+                visited[links[i][1]] = true;
             }
         }
         
@@ -30,50 +28,50 @@ class Solution {
             }
         }
         
-        int l = 0;
-        int r = 0;
-        for(int number : num) {
-            l = Math.max(l, number);
-            r += number;
+        int L = 0;
+        int R = 0;
+        for(int i=0; i<num.length; i++) {
+            L = Math.max(L, num[i]);
+            R += num[i];
         }
         
-        int answer = 0;
-        while(l<=r) {
-            int mid = (l+r)/2;
+        answer = 0;
+        while(L <= R) {
+            int mid = (L + R)/2;
             if(check(mid) <= k) {
                 answer = mid;
-                r = mid - 1;
+                R = mid - 1;
             }else {
-                l = mid + 1;
+                L = mid + 1;
             }
         }
         
         return answer;
     }
     
-    public int check(int val) {
+    public int check(int max) {
         size = 0;
-        DFS(root, val);
+        DFS(root, max);
         return size + 1;
     }
     
-    public int DFS(int node, int val) {
+    public int DFS(int node, int max) {
         int left = 0;
         int right = 0;
-        if(links[node][0] != -1)  {
-            left = DFS(links[node][0], val);
-        }
+        if(links[node][0] != -1) {
+            left = DFS(links[node][0], max);
+        } 
         
         if(links[node][1] != -1) {
-            right = DFS(links[node][1], val);
+            right = DFS(links[node][1], max);
         }
-               
-        if(num[node] + left + right <= val) {
+        
+        if(num[node] + left + right <= max) { // 안 잘라도됨.
             return num[node] + left + right;
         }
         
-        if(num[node] + Math.min(left, right) <= val) {
-            size += 1;
+        if(num[node] + Math.min(left, right) <= max) { // 큰쪽을 자름
+            size +=1;
             return num[node] + Math.min(left, right);
         }
         
