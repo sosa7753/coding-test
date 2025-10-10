a2ed776dc1229c57d886516a4fb7949a46e9af6c
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-10 21:04:28 +0900

    [level 4] Title: 징검다리, Time: 39.67 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,52 +1,48 @@
 import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
-        if(rocks.length == n) {
+        int r = rocks.length;
+        
+        if(r == n) {
             return distance;
         }
-                
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0; i<rocks.length; i++) {
-            pq.add(rocks[i]);
+        
+        Arrays.sort(rocks);
+        int[] dist = new int[r+1];  
+        for(int i=0; i<r; i++) {
+            dist[i] = rocks[i];
         }
+        dist[r] = distance; // 2 11 14 17 21 25
         
-        int[] arr = new int[rocks.length+1];
-        int idx = 0;
-        while(!pq.isEmpty()) {
-            arr[idx++] = pq.poll();
-        }
-        
-        arr[idx] = distance;
-        
-        int left = 1;
-        int right = distance -1;
-        
-        while(left <= right) {
-            int mid = right - (right - left)/2; // 최솟값
-            
-            boolean isfalse = false; // true면 불가능 
-            int remove = n;
-            int pre = 0;
-            for(int i=0; i<arr.length; i++) {
-                if(arr[i] - pre < mid) {
-                    if(remove == 0) {
-                        isfalse = true; 
-                        break;
-                    }else {
-                        remove--;
-                    }
-                }else {
-                    pre = arr[i];
-                }
-            }
-                       
-            if(isfalse) { // 불가능하면, 더 작은 값으로 찾아야함.
-                right = mid -1;
+        int answer = 0;
+        int L = 0;
+        int R = distance;
+        while(L <= R) {
+            int mid = (L+R)/2;
+            if(check(mid, dist, n)) {
+                answer = mid;
+                L = mid + 1;
             }else {
-                left = mid + 1;
+                R = mid - 1;
             }
         }
-        
-        return left - 1;
+        return answer;
     }
+    
+    public boolean check(int val, int[] dist, int n) {
+        int cnt = n;
+        int pre = 0;
+        for(int i=0; i<dist.length; i++) {
+            if(val > dist[i] - pre) {
+                if(cnt == 0) {
+                    return false;
+                }else {
+                    cnt--;
+                }
+            }else {
+                pre = dist[i];
+            }
+        }
+        return true;
+    }  
 }
