dc9bb672f17690e08f84d40fc70b90d937a280fb
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-11 00:14:01 +0900

    [level 4] Title: 사칙연산, Time: 17.14 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,69 +1,71 @@
 class Solution {
+    int[][][] dp;
     int[] num;
-    boolean[] operator; // true는 +, false는 -;
-    int[][][] dp; // start, end, 최대/최소 = 0/1
-    public int solution(String arr[]) {
-        num = new int[arr.length/2 + 1];
-        operator = new boolean[arr.length/2];
-        div(arr);
+    char[] oper;
+    public int solution(String arr[]) {    
+        // dp로 s ~ e까지의 최댓값 최솟값 구하기 
+        // 메모이제이션 활용하기
+        init(arr);
+        int len = num.length;
+        dp = new int[len][len][2]; // 0은 최댓값, 1은 최솟값
         
-        dp = new int[num.length][num.length][2];
-        for(int i=0; i<num.length; i++) {
-            for(int j=i; j<num.length; j++) {
-                if(i==j) {
+        for(int i=0; i<len; i++) {
+            for(int j=0; j<len; j++) {
+                if(i == j) {
                     dp[i][j][0] = num[i];
                     dp[i][j][1] = num[i];
+                    continue;
+                }
+                dp[i][j][0] = Integer.MIN_VALUE;
+                dp[i][j][1] = Integer.MAX_VALUE;
+            }
+        }
+           
+        return DFS(0, len-1, 0);
+    }
+    
+    public int DFS(int s, int e, int flag) {
+        if(dp[s][e][flag] != Integer.MIN_VALUE && 
+           dp[s][e][flag] != Integer.MAX_VALUE) {
+            return dp[s][e][flag];
+        }
+        
+        if(s == e) {
+            return dp[s][e][flag];
+        }
+        
+        for(int i=s; i<e; i++) {
+            if(flag == 0) {
+                if(oper[i] == '+') { // 최댓값 구하기
+                    dp[s][e][flag] = Math.max(dp[s][e][flag],
+                                              DFS(s, i, 0) + DFS(i+1, e, 0));
                 }else {
-                    dp[i][j][0] = Integer.MIN_VALUE;
-                    dp[i][j][1] = Integer.MAX_VALUE;
+                    dp[s][e][flag] = Math.max(dp[s][e][flag], 
+                                              DFS(s, i, 0) - DFS(i+1, e, 1));
+                }
+            }else {
+                if(oper[i] == '+') { // 최솟값
+                    dp[s][e][flag] = Math.min(dp[s][e][flag],
+                                              DFS(s, i, 1) + DFS(i+1, e, 1));
+                }else {
+                    dp[s][e][flag] = Math.min(dp[s][e][flag], 
+                                              DFS(s, i, 1) - DFS(i+1, e, 0));
                 }
             }
         }
-        
-        return DP(0, num.length-1, 0);
+        return dp[s][e][flag];
     }
     
-    public int DP(int start, int end, int flag) {
-        if(visit(start, end, flag)) {
-            return dp[start][end][flag];
-        }
-        
-        for(int i=start; i<end; i++) {
-            if(flag == 0) { // 최대 = 앞에 최대 고정 뒤에는 + 면 최대, -면 최소
-                if(operator[i]) { // 다음이 +다
-                    dp[start][end][flag] = Math.max(dp[start][end][flag], 
-                                                    DP(start, i, 0) + DP(i+1, end, 0));
-                }else {
-                    dp[start][end][flag] = Math.max(dp[start][end][flag],
-                                                   DP(start, i, 0) - DP(i+1, end, 1));
-                }              
-            }else { // 최소 = 앞에 최소 고정 뒤에는 + 면 최소, -면 최대 
-                if(operator[i]) { // 다음이 +다
-                    dp[start][end][flag] = Math.min(dp[start][end][flag], 
-                                                    DP(start, i, 1) + DP(i+1, end, 1));
-                }else {
-                    dp[start][end][flag] = Math.min(dp[start][end][flag],
-                                                   DP(start, i, 1) - DP(i+1, end, 0));
-                } 
-            }
-        }
-        return dp[start][end][flag];
-    }
-    
-    public void div(String[] arr) {
+    public void init(String[] arr) {
+        num = new int[arr.length/2 + 1];
+        oper = new char[arr.length/2];
+
         for(int i=0; i<arr.length; i++) {
-            if(i%2 ==0) {
+            if(i%2 == 0) {
                 num[i/2] = Integer.parseInt(arr[i]);
             }else {
-                if("+".equals(arr[i])) {
-                    operator[i/2] = true;
-                }
+                oper[i/2] = arr[i].charAt(0);
             }
         }
-    } 
-    
-    public boolean visit(int start, int end, int flag) {
-        return dp[start][end][flag] != Integer.MAX_VALUE &&
-               dp[start][end][flag] != Integer.MIN_VALUE;
     }
 }
