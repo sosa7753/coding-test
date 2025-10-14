97f764e128943933463d98bb097ebf45fcf82685
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-14 16:17:34 +0900

    [level 5] Title: 방의 개수, Time: 178.47 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.java
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.java
@@ -4,36 +4,33 @@ class Solution {
     int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
     public int solution(int[] arrows) {
         int answer = 0;
+        
         Map<String, Set<Integer>> map = new HashMap<>();
-    
-        int curX = 0;
-        int curY = 0;
-        String now = key(curX, curY);
+        int x = 0; int y = 0; String now = makeKey(x, y);
         map.put(now, new HashSet<>());
         for(int i=0; i<arrows.length; i++) {
             for(int j=0; j<2; j++) {
-                int nx = curX + dx[arrows[i]];
-                int ny = curY + dy[arrows[i]];
-                String s = key(nx, ny);
-                              
-                if(!map.containsKey(s)) { // 첫 방문하는 점
-                    map.put(s, new HashSet<>());                    
-                }else if(!map.get(now).contains(arrows[i])) { // 처음 보는 간선
+                int nx = x + dx[arrows[i]];
+                int ny = y + dy[arrows[i]];
+                String nextKey = makeKey(nx, ny);
+                
+                if(!map.containsKey(nextKey)) {
+                    map.put(nextKey, new HashSet<>());
+                }else if(!map.get(now).contains(arrows[i])) {
                     answer++;
                 }
                 
-                map.get(s).add((arrows[i]+4)%8); // 역방향
-                map.get(now).add(arrows[i]); // 정방향
-                curX = nx;
-                curY = ny;
-                now = s;       
+                map.get(nextKey).add((arrows[i]+4)%8);
+                map.get(now).add(arrows[i]);
+                x = nx; y = ny; 
+                now = nextKey;
             }
-        } 
+        }
         
         return answer;
     }
     
-    public String key(int x, int y) {
+    public String makeKey(int x, int y) {
         StringBuilder sb = new StringBuilder();
         return sb.append(x).append(",").append(y).toString();
     }
