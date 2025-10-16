b160d27192d543de7bcd2c3e70079c401d34f8af
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-16 20:55:28 +0900

    [level 4] Title: 지형 이동, Time: 116.33 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.java
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.java
@@ -2,53 +2,63 @@ import java.util.*;
 class Solution {
     int[] dr = {-1, 0, 1, 0};
     int[] dc = {0, 1, 0, -1};
-    boolean[][] visited;    
-    PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[2] - y[2]));
-    int answer = 0;    
+    int answer = 0;
+    int height;
+    int n;
+    int[][] land;
+    boolean[][] visited;
+    PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[2]-y[2]));
     public int solution(int[][] land, int height) {
-        visited = new boolean[land.length][land[0].length];        
-        pq.offer(new int[] {0,0,0});
+        this.n = land.length;
+        this.height = height;
+        this.land = land;
+        visited = new boolean[n][n];
         
-        while(!pq.isEmpty()) {      
-            int[] now = pq.poll();            
-            if(visited[now[0]][now[1]]) {
-                continue;
-            }
+        pq.offer(new int[]{0,0,0});
+        while(!pq.isEmpty()) {
+            int[] now = pq.poll();
+            int r = now[0]; int c = now[1]; int h = now[2];
             
-            answer += now[2];
-            BFS(now[0], now[1], land, height);
+            if(visited[r][c]) continue;
+            
+            visited[r][c] = true;
+            answer += h;
+            
+            BFS(r, c);
             
         }
+        
         return answer;
     }
     
-    public void BFS(int row, int col, int[][] land, int height) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {row, col});
-        visited[row][col] = true;
-      
-        while(!queue.isEmpty()) {
-            int[] now = queue.poll();
-                               
+    public void BFS(int r, int c){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r, c});
+               
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            int row = now[0]; int col = now[1]; 
+            
             for(int i=0; i<4; i++) {
-                int r = now[0] + dr[i];
-                int c = now[1] + dc[i];
+                int nr = row + dr[i];
+                int nc = col + dc[i];
                 
-                if(r < 0 || r > land.length-1 || c < 0 || c > land.length-1) {
+                if(nr < 0 || nr > n-1 || nc < 0 || nc > n-1) {
+                    continue;
+                }
+                                
+                if(visited[nr][nc]) {
                     continue;
                 }
                 
-                if(visited[r][c]) {
-                    continue;
-                }             
-                int cur = land[now[0]][now[1]]; // 현재 값
-                if(land[r][c] >= cur - height && land[r][c] <= cur+height) { 
-                    queue.offer(new int[] {r, c});
-                    visited[r][c] = true;  
-                }else { // 불가능
-                    pq.offer(new int[] {r,c, Math.abs(land[r][c] - cur)});
+                int gap = Math.abs(land[nr][nc] - land[row][col]);
+                if(gap <= height) {
+                    q.offer(new int[]{nr, nc});
+                    visited[nr][nc] = true;
+                }else {
+                    pq.offer(new int[]{nr, nc, gap});
                 }
             }
-        } 
-    }    
-}
+        }
+    }
+ }
