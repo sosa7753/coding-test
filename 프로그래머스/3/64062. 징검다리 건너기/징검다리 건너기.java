699f9431512bad7eb448c64684e7d5291ee45418
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2025-10-17 22:17:01 +0900

    [level 3] Title: 징검다리 건너기, Time: 62.91 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,42 +1,32 @@
+import java.util.*;
 class Solution {
     public int solution(int[] stones, int k) {
-        int answer = 0;
+        int answer = Integer.MAX_VALUE;
         
-        // 이진탐색으로 몇 명을 정하기
-        // stone[i]-mid 만큼 빼주자. 그때 k이상의 0이하인 구간이 생기면 실패
-        
-        int L = 0;
-        int R = Integer.MAX_VALUE;
-        while(L<=R) {
-            int mid = (L+R)/2;
-            
-            if(check(stones, k , mid)) {
-                answer = mid;
-                L = mid + 1;
-            }else {
-                R = mid - 1;
-            }
-        }
-        return answer + 1;
-    }
-    
-    public boolean check(int[] stones, int k, int mid) {
-        int gap = 0;
+        Deque<Node> dq = new LinkedList<>();        
         for(int i=0; i<stones.length; i++) {
-            if(gap >= k) {
-                return false;
+            while(!dq.isEmpty() && dq.getLast().value < stones[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(new Node(stones[i], i));
+            
+            if(dq.peek().idx <= i-k) { // dq를 비우지 않았을 때 4(3) 
+                dq.poll();
             }
             
-            if(stones[i] - mid <= 0) {
-                gap++;
-            }else {
-                gap = 0;
-            }
-         }
-        
-         if(gap >= k) {
-             return false;
-         }
-         return true;
+            if(dq.getLast().idx >= k-1) {
+                answer = Math.min(answer, dq.peek().value);
+            }        
+        }
+        return answer;
+    }
+}
+
+class Node {
+    int value;
+    int idx;
+    Node(int value, int idx) {
+        this.idx = idx;
+        this.value = value;
     }
 }
