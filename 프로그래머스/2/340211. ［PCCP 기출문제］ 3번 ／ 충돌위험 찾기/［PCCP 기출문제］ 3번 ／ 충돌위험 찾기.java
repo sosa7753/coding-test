78f9989dfe846a226c7f79f292b4451fd5953499
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2026-07-02 17:51:17 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 205.95 ms, Memory: 201 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class Solution {
+    Map<Integer, Map<String, Integer>> map = new HashMap<>();
+    public int solution(int[][] points, int[][] routes) {    
+        map.put(0, new HashMap<>());
+        for(int[] route : routes) { // 각 루트 경로에 대해 
+            int idx = 0;
+            insert(points[route[0]-1][0], points[route[0]-1][1], idx);
+            for(int i=0; i<route.length-1; i++) { // i와 i+1번째 경로     
+                int nextIdx = move(route[i], route[i+1], points, idx);                           idx = nextIdx;
+            }
+        }
+        
+        int answer = 0;
+        for(Map<String, Integer> m : map.values()) {
+            for(int v : m.values())  {
+                if(v >=2) answer++;
+            }
+        }
+        return answer;
+    }
+    
+    public int move(int s, int e, int[][] points, int startTime) {
+        int sr = points[s-1][0];
+        int sc = points[s-1][1];
+        int er = points[e-1][0];
+        int ec = points[e-1][1];
+        
+        int idx = startTime;
+        while(sr != er || sc != ec) {
+            if(sr == er) {
+                sc = (ec > sc) ? sc+1 : sc-1;
+            }else {
+                sr = (er > sr) ? sr+1 : sr-1;
+            }
+            idx++;
+            insert(sr, sc, idx);
+        }
+        return idx;
+    }
+    
+    public String inverter(int r, int c) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(r).append(" ").append(c).toString();
+    }
+    
+    public void insert(int r, int c, int idx) {
+        if(!map.containsKey(idx)) {
+            map.put(idx, new HashMap<>());
+        }
+            
+        String key = inverter(r, c);
+        Map<String, Integer> tmp = map.get(idx);
+        tmp.put(key, tmp.getOrDefault(key, 0) + 1);
+    }
+}
