ac0844f949ddb7c393739c75213ca49cc2ee7f3c
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2026-07-06 15:39:13 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 1.52 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -0,0 +1,105 @@
+class Solution {
+    int[] dr = {-1, 0, 1, 0};
+    int[] dc = {0, 1, 0, -1};
+    int answer = Integer.MAX_VALUE;
+    int err = 0; int erc = 0; // 빨 도착
+    int ebr = 0; int ebc = 0; // 파 도착
+    int n, m;
+    public int solution(int[][] maze) {
+        n = maze.length; m = maze[0].length;       
+        int srr = 0; int src = 0;
+        int sbr = 0; int sbc = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(maze[i][j] == 1) { 
+                    srr = i; src = j;
+                }else if(maze[i][j] == 2) {
+                    sbr = i; sbc = j;
+                }else if(maze[i][j] == 3) {
+                    err = i; erc = j;
+                }else if(maze[i][j] == 4) {
+                    ebr = i; ebc = j;
+                }
+            }
+        }
+        
+        boolean[][] visitedR = new boolean[n][m];
+        boolean[][] visitedB = new boolean[n][m];
+        visitedR[srr][src] = true;
+        visitedB[sbr][sbc] = true;
+        DFS(maze, visitedR, visitedB, new int[]{srr, src}, new int[]{sbr, sbc}, 0);
+        if(answer == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return answer;
+    }
+    public void DFS(int[][] maze, boolean[][] visitedR, boolean[][] visitedB, 
+                    int[] R, int[] B, int cnt) {
+         if(cnt >= answer) {
+             return;
+         }
+        
+         int r1 = R[0]; int c1 = R[1]; int r2 = B[0]; int c2 = B[1];
+         if(r1 == err && c1 == erc && r2 == ebr && c2 == ebc) {
+             answer = Math.min(answer, cnt);
+             return;
+         }
+        
+         // 동시에 같은 칸 못감
+         // 수레끼리 자리를 바꾸며 가는 걸 못함
+        
+         // 빨간 수레 고정
+         if(r1 == err && c1 == erc) {
+             for(int i=0; i<4; i++) {
+                 int nr2 = r2 + dr[i];
+                 int nc2 = c2 + dc[i]; 
+                 if(!check(maze, visitedB, nr2, nc2)) continue;
+                 if(r1 == nr2 && c1 == nc2) continue;
+                 visitedB[nr2][nc2] = true;
+                 DFS(maze, visitedR, visitedB, R, new int[]{nr2, nc2}, cnt+1);
+                 visitedB[nr2][nc2] = false;
+             }
+         }
+        
+         // 파란 수레 고정 
+         if(r2 == ebr && c2 == ebc) {
+             for(int i=0; i<4; i++) {
+                 int nr1 = r1 + dr[i];
+                 int nc1 = c1 + dc[i]; 
+                 if(!check(maze, visitedB, nr1, nc1)) continue;
+                 if(r2 == nr1 && c2 == nc1) continue;
+                 visitedB[nr1][nc1] = true;
+                 DFS(maze, visitedR, visitedB, new int[]{nr1, nc1}, B, cnt+1);
+                 visitedB[nr1][nc1] = false;
+             }
+         }
+         
+         // 모두 안맞을 경우
+         for(int i=0; i<4; i++) { // 빨강의 4방향
+             int nr1 = r1 + dr[i];
+             int nc1 = c1 + dc[i];
+             if(!check(maze, visitedR, nr1, nc1)) continue;            
+             visitedR[nr1][nc1] = true;
+             for(int j=0; j<4; j++) { // 파랑의 4방향
+                 int nr2 = r2 + dr[j];
+                 int nc2 = c2 + dc[j];
+                 if(!check(maze, visitedB, nr2, nc2)) continue;
+                 if(nr1 == nr2 && nc1 == nc2) continue; // 같은 자리
+                 if(r1 == nr2 && c1 == nc2 && r2 == nr1 && c2 == nc1) continue; // 스왑
+                 visitedB[nr2][nc2] = true;
+                 DFS(maze, visitedR, visitedB, 
+                     new int[]{nr1, nc1}, new int[]{nr2, nc2}, cnt+1);
+                 visitedB[nr2][nc2] = false;
+             }
+             visitedR[nr1][nc1] = false;
+         }
+    }
+    
+    // 맵 밖 + 벽 + 방문한 곳인지 -> true면 가능
+    public boolean check(int[][] maze, boolean[][] visited, int r, int c) {
+        if(r < 0 || r > n-1 || c < 0 || c > m-1) return false;
+        if(maze[r][c] == 5) return false;
+        if(visited[r][c]) return false;
+        return true;
+    }
+}
