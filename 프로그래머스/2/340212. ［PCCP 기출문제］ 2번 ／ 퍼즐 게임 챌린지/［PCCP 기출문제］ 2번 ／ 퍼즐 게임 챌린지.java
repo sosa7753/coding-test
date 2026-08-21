8a888d067de2769fc259d75869efbdc4a86c292d
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2026-08-21 21:15:54 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 30.75 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,40 +1,34 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int left = 1;
-        int right= 1;
-        for(int i=0; i<diffs.length; i++) {
-            right = Math.max(diffs[i], right);
-        }
+        int answer = Integer.MAX_VALUE;
         
-        int answer = 0;
-        while(left <= right) {
-            int mid = (left + right)/2;
-            
-            if(isPossible(diffs, times, limit, mid)) {
-                right = mid - 1;
-                answer = mid;
+        int R = 0;
+        for(int d : diffs) {
+            R = Math.max(R, d);
+        }
+        int L = 1;
+        while(L<=R) {
+            int mid = L + (R - L)/2;
+            if(solve(diffs, times, mid, limit)) {
+                answer = Math.min(answer, mid);   
+                R = mid - 1;
             }else {
-                left = mid + 1;
+                L = mid + 1;
             }
         }
-        
         return answer;
     }
     
-    public boolean isPossible(int[] diffs, int[] times, long limit, int level) {
-        long value = 0;
-        long pre = 0;
-        for(int i=0; i<diffs.length; i++) {
-            if(level < diffs[i]) {
-                value += (diffs[i] - level) * (pre + times[i]);
+    public boolean solve(int[] diffs, int[] times, int v, long limit) {
+        long result = times[0];
+        for(int i=1; i<diffs.length; i++) {
+            if(diffs[i] <= v) {
+                result += times[i];
+            }else {
+                result += (times[i-1] + times[i]) * (diffs[i] - v) + times[i];
             }
-            value += times[i];
-            pre = times[i];
         }
         
-        if(limit >= value) {
-            return true;
-        }
-        return false;
+        return result <= limit ? true : false;
     }
 }
