bc509c6ee734fc4626c0bdd95cf33e35a33b93d2
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2026-08-21 22:05:30 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 46.76 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,36 +1,40 @@
 import java.util.*;
 class Solution {
-    int n, m;
-    int[][] board;
-    Map<Integer, Integer> map = new HashMap<>();
-    public int solution(int[][] land) {
-        n = land.length; m = land[0].length; 
-        board = land;
+    Map<Integer, Integer> map = new HashMap<>(); // 각 열 인덱스의 값
+    int[][] land;
+    int n,m;
+    public int solution(int[][] land) {      
+        this.land = land;
+        n = land.length;
+        m = land[0].length;
         
         int idx = 2;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(board[i][j] != 1) continue;
-                BFS(i, j, idx);
-                idx++;
+                if(land[i][j]==1) {
+                   BFS(i, j, idx);
+                   idx++; 
+                }
             }
         }
-        
+                 
         int answer = 0;
         for(int v : map.values()) {
             answer = Math.max(answer, v);
         }
+       
         return answer;
     }
     
     public void BFS(int row, int col, int idx) {
         int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, 1, 0, -1}; 
+        int[] dc = {0, 1, 0, -1};
+        
+        land[row][col] = idx;
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{row, col});
-        board[row][col] = idx;
-                
-        Set<Integer> set = new HashSet<>();      
+        q.offer(new int[]{row, col});
+
+        Set<Integer> set = new HashSet<>();
         int cnt = 0;
         while(!q.isEmpty()) {
             int[] now = q.poll();
@@ -41,16 +45,21 @@ class Solution {
             for(int i=0; i<4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
+                
                 if(nr < 0 || nr > n-1 || nc < 0 || nc > m-1) continue;
-                if(board[nr][nc] != 1) continue;
-                board[nr][nc] = idx;
-                q.offer(new int[]{nr, nc});
+                if(land[nr][nc] == 1) {
+                    land[nr][nc] = idx;
+                    q.offer(new int[]{nr, nc});          
+                }           
             }
         }
         
         for(int s : set) {
-            if(!map.containsKey(s)) map.put(s, cnt);
-            else map.put(s, map.get(s) + cnt);
+            if(!map.containsKey(s)) {
+                map.put(s, cnt);
+            }else {
+                map.put(s, map.get(s) + cnt);
+            }
         }
     }
 }
