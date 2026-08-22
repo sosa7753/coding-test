43683ba2e0946d5c609e46ca8f93565b6088b48b
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2026-08-22 16:04:22 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 1.62 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -1,103 +1,63 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] expressions) {
-        Set<Integer> set = new HashSet<>(); // 가능한 진법만 남기기
-        
         int max = 0;
-        List<String> listX = new ArrayList<>();
-        List<String> nonX = new ArrayList<>();
+        for(String e : expressions) { // 등장하는 숫자들 중 최댓값 찾기
+            for(char c : e.toCharArray()) {
+                if(Character.isDigit(c)) max = Math.max(max, c-'0');
+            }
+        }
+        
+        List<Integer> valid = new ArrayList<>(); // 가능한 진법만 모으기
+        for(int i = max+1; i<=9; i++) {
+            if(isValid(i, expressions)) valid.add(i);
+        }
+     
+        List<String> answer = new ArrayList<>();
         for(String e : expressions) {
-            if(e.charAt(e.length()-1) == 'X') listX.add(e);
-            else nonX.add(e);
-            
-            max = Math.max(max, maxNumber(e));              
+            String[] str = e.split(" ");
+            if(!str[4].equals("X")) continue;
+            answer.add(e.replace("X", resolve(str, valid)));
         }
         
-        for(int i=max+1; i<=9; i++) {
-            set.add(i);
-        }
-        
-        // 가능한 진법 체크
-        boolean get = false;
-        for(String e : nonX) {
-            if(get) break;
-            for(int i=max+1; i<=9; i++) {
-                if(set.size() == 1) {
-                    get = true;
-                    break;
-                }
-                
-                if(!set.contains(i)) continue;
-                
-                String[] str = e.split(" ");
-                int one = toTen(i, Integer.parseInt(str[0]));
-                int two = toTen(i, Integer.parseInt(str[2]));
-                int three = toTen(i, Integer.parseInt(str[4]));
-                if((str[1].equals("+") && (one + two == three)) || 
-                   str[1].equals("-") && (one - two == three)) {
-                    continue;
-                }
-                set.remove(i);
-            }
-        }
-        
-        String[] answer = new String[listX.size()];
-        for(int i=0; i<listX.size(); i++) { // set을 순회해서 찾기
-            answer[i] = cal(set, listX.get(i));
-        }
-        
-        return answer;
+        return answer.toArray(new String[0]);
     }
     
-    public String cal(Set<Integer> set, String s) {
-        Set<String> result = new HashSet<>();
-        String[] str = s.split(" ");
-        
-        for(int t : set) {
-            int one = toTen(t, Integer.parseInt(str[0]));
-            int two = toTen(t, Integer.parseInt(str[2]));
-            if(str[1].equals("+")) result.add(fromTen(t, (one + two)));
-            else result.add(fromTen(t, (one - two)));
+    public boolean isValid(int op, String[] expressions) {
+        for(String e : expressions) {
+            String[] str = e.split(" ");
+            if(str[4].equals("X")) continue;
+            int cal = calc(str, op);
+            if(cal != toTen(str[4], op)) return false;
         }
-        
-        if(result.size() == 1) {
-            for(String r : result) return s.replace("X", r);
-        }
-        return s.replace("X", "?");
+        return true;
     }
     
-    public int maxNumber(String s) {
-        int max = 0; 
-        String[] str = s.split(" ");
-        for(int i=0; i<=4; i=i+2) {
-            if(str[i].equals("X")) break;
-            
-            for(char c : str[i].toCharArray()) {
-                max = Math.max(max, c - '0');
-            }
-        }
-        return max;
+    public String resolve(String[] str, List<Integer> valid) {
+        Set<String> results = new HashSet<>();
+        for(int l : valid) results.add(fromTen(calc(str, l), l));
+        return results.size() == 1 ? results.iterator().next() : "?";
     }
     
-    public int toTen(int cnt, int v) { // cnt 진법 -> 10진법
-       int result = 0; 
-       int idx = 1;
-       while(v != 0) {
-           result += (v%10) * idx;
-           v /= 10;
-           idx *= cnt;
-       }
+    public int calc(String[] str, int op) {
+        int a = toTen(str[0], op); 
+        int b = toTen(str[2], op);
+        return str[1].equals("+") ? a+b : a-b;
+    }
+    
+    public int toTen(String num, int op) { // 한 칸씩 밀고, op 진법을 곱해주는 방법
+        int result = 0;
+        for(char c : num.toCharArray()) result = result * op + (c - '0');
         return result;
     }
     
-    public String fromTen(int cnt, int v) { // 10진법 -> cnt 진법 변환
+    public String fromTen(int v, int op) {
         if(v == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-        int idx = 1;
-        while(v != 0) {
-            sb.append(v%cnt);
-            v /= cnt;
+        StringBuilder sb =  new StringBuilder();
+        while(v > 0) {
+            sb.append(v%op);
+            v /= op;
         }
-        return sb.reverse().toString();  
+        return sb.reverse().toString();
     }
 }
