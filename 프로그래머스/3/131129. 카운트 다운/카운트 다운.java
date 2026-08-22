a23a21d49ea6c0dd566f20fb7d36b22a12905dad
sosa7753 <141195262+sosa7753@users.noreply.github.com>
2026-08-22 20:31:33 +0900

    [level 3] Title: 카운트 다운, Time: 16.13 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -2,30 +2,29 @@ import java.util.*;
 class Solution {
     public int[] solution(int target) {
         int[] dart = new int[target+1]; // 최소 다트수
-        int[] dp = new int[target+1]; // 최대 싱글+불 수 
-        
+        int[] dp = new int[target+1]; // 최대 싱글 + 볼 수
         Arrays.fill(dart, Integer.MAX_VALUE);
         dart[0] = 0;
         
         for(int i=1; i<=target; i++) {
-            if(i >= 50) {
+            if(i>=50) {
                 if(dart[i] > dart[i-50] + 1) {
                     dart[i] = dart[i-50] + 1;
                     dp[i] = dp[i-50] + 1;
-                }else if(dart[i] == dart[i-50] + 1) {
+                }else if(dart[i] == dart[i-50]+1) {
                     dp[i] = Math.max(dp[i], dp[i-50] + 1);
-                }           
+                }
             }
             
             for(int j=1; j<=20; j++) {
-                if(i >= 3 * j) {
+                if(i >= 3*j) {
                     if(dart[i] > dart[i-3*j] + 1) {
                         dart[i] = dart[i-3*j] + 1;
                         dp[i] = dp[i-3*j];
                     }
                 }
                 
-                if(i >= 2 * j) {
+                if(i >= 2*j) {
                     if(dart[i] > dart[i-2*j] + 1) {
                         dart[i] = dart[i-2*j] + 1;
                         dp[i] = dp[i-2*j];
@@ -40,7 +39,7 @@ class Solution {
                         dp[i] = Math.max(dp[i], dp[i-j] + 1);
                     }
                 }
-            }    
+            }
         }
         
         int[] answer = new int[2];
